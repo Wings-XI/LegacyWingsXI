7e8cab2edf68ad596e272408b7f6d96c71cb3fde
Gweivyth Quartet <admin@mageringnetwork.com>
2021-01-30 18:27:37 +0000

    Update scripts/zones/Carpenters_Landing/mobs/Overgrown_Ivy.lua

--- scripts/zones/Carpenters_Landing/mobs/Overgrown_Ivy.lua
+++ scripts/zones/Carpenters_Landing/mobs/Overgrown_Ivy.lua
@@ -9,6 +9,6 @@ end
 
 function onMobDeath(mob, player, isKiller)
     if (player:getCurrentMission(COP) == tpz.mission.id.cop.THE_ROAD_FORKS and player:getCharVar("EMERALD_WATERS_Status") == 4) then
-        player:setCharVar("EMERALD_WATERS_Status",5);
+        player:setCharVar("EMERALD_WATERS_Status", 5)
     end
 end;
