30326753486cb40c84fb1c7d56b3b807663b5484
FFXI Wings <ffxiwings@outlook.com>
2021-01-30 09:08:59 -0500

    Gravity spell fix

--- src/map/ai/helpers/pathfind.cpp
+++ src/map/ai/helpers/pathfind.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
 ===========================================================================
 
 Copyright (c) 2010-2015 Darkstar Dev Teams
@@ -425,7 +425,8 @@ float CPathFind::GetRealSpeed()
 
     // Lets not factor in player map conf or mod's to non players. 
     // (Mobs should just have speed set directly instead, and NPC's don't have mods)
-    if (m_PTarget->objtype == TYPE_PC)
+    // Twilight: Let's revert that so we don't break the gravity spell when cast on mobs
+    if (m_PTarget->objtype != TYPE_NPC)
     {
         baseSpeed = ((CBattleEntity*)m_PTarget)->GetSpeed();
     }
