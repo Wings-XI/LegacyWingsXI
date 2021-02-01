717def999c867ec8c10f7caf31bed82b70bf71e8
FFXI Wings <ffxiwings@outlook.com>
2021-01-31 19:21:39 -0500

    Fix crash on additem

--- src/map/treasure_pool.cpp
+++ src/map/treasure_pool.cpp
@@ -174,7 +174,7 @@ uint8 CTreasurePool::AddItem(uint16 ItemID, CBaseEntity* PEntity)
         for (SlotID = 0; SlotID < 10; ++SlotID)
         {
             CItem* PItem = itemutils::GetItemPointer(m_PoolItems[SlotID].ID);
-            if (!(PItem->getFlag() & (ITEM_FLAG_RARE | ITEM_FLAG_EX)) && m_PoolItems[SlotID].TimeStamp < oldest)
+            if ((PItem) && (!(PItem->getFlag() & (ITEM_FLAG_RARE | ITEM_FLAG_EX)) && m_PoolItems[SlotID].TimeStamp < oldest))
             {
                 FreeSlotID = SlotID;
                 oldest = m_PoolItems[SlotID].TimeStamp;
@@ -186,7 +186,7 @@ uint8 CTreasurePool::AddItem(uint16 ItemID, CBaseEntity* PEntity)
             for (SlotID = 0; SlotID < 10; ++SlotID)
             {
                 CItem* PItem = itemutils::GetItemPointer(m_PoolItems[SlotID].ID);
-                if (!(PItem->getFlag() & (ITEM_FLAG_EX)) && m_PoolItems[SlotID].TimeStamp < oldest)
+                if ((PItem) && (!(PItem->getFlag() & (ITEM_FLAG_EX)) && m_PoolItems[SlotID].TimeStamp < oldest))
                 {
                     FreeSlotID = SlotID;
                     oldest = m_PoolItems[SlotID].TimeStamp;
