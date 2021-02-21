22aa1d17237c9945e594bf9084e5616a949f2844
Twilight <23407689+NiQ1@users.noreply.github.com>
2021-02-21 22:06:23 +0200

    Jump emote missing files added

---
+++ src/map/packets/char_jump.h
@@ -0,0 +1,28 @@
+﻿/*
+===========================================================================
+
+  Copyright (c) 2021 Wings Project
+
+  Use of this file is under AGPLv3, see LICENSE file.
+  
+  Author: Twilight
+
+===========================================================================
+*/
+
+#ifndef _CCHARJUMPPACKET_H
+#define _CCHARJUMPPACKET_H
+
+#include "../../common/cbasetypes.h"
+
+#include "basic.h"
+
+class CCharEntity;
+
+class CCharJumpPacket : public CBasicPacket
+{
+public:
+    CCharJumpPacket(CCharEntity* PChar, uint32 extra);
+};
+
+#endif
