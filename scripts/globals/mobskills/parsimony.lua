111928b535629e5a5573eb7eb88333bbba53f5fd
mgramolini <mattygs@gmail.com>
2021-02-23 00:40:59 -0500

    WIP - Scholar AF3  (debug phase1)

---
+++ scripts/globals/mobskills/parsimony.lua
@@ -0,0 +1,19 @@
+---------------------------------------------
+-- Parsimony
+---------------------------------------------
+require("scripts/globals/monstertpmoves")
+require("scripts/globals/status")
+require("scripts/globals/msg")
+---------------------------------------------
+
+function onMobSkillCheck(target, mob, skill)
+    return 0
+end
+
+function onMobWeaponSkill(target, mob, skill)
+    MobBuffMove(mob, tpz.effect.PARSIMONY, 1, 0, 60)
+
+    skill:setMsg(tpz.msg.basic.USES)
+
+    return tpz.effect.PARSIMONY
+end
