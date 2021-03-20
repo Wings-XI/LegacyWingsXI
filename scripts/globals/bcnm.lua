a516c5d82f6042dd302928dabfd452dd500a9279
FFXI Wings <ffxiwings@outlook.com>
2021-03-20 21:58:12 +0100

    Disable Sheep in Antilon's Clothing until properly tuned.

--- scripts/globals/bcnm.lua
+++ scripts/globals/bcnm.lua
@@ -31,7 +31,7 @@ local battlefields = {
     {
         { 0,  672,    0},   -- Head Wind (PM5-3 U2)
      -- { 1,  673,    0},   -- Like the Wind (ENM) -- TODO: mob constantly runs during battle
-        { 2,  674,    0},   -- Sheep in Antlion's Clothing (ENM)
+     -- { 2,  674,    0},   -- Sheep in Antlion's Clothing (ENM)
      -- { 3,  675,    0},   -- Shell We Dance? (ENM) -- TODO: Needs testing, cleanup, and mixin work
      -- { 4,  676,    0},   -- Totentanz (ENM)
      -- { 5,  677,    0},   -- Tango with a Tracker (Quest)
