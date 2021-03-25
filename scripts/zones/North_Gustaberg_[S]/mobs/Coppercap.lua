93489b43f487cbb83a4e38d17d13baf3493c7cd7
Critical <lyardeen@gmail.com>
2021-03-25 16:13:33 -0600

    Coppercap Regen/Regain added

--- scripts/zones/North_Gustaberg_[S]/mobs/Coppercap.lua
+++ scripts/zones/North_Gustaberg_[S]/mobs/Coppercap.lua
@@ -5,7 +5,22 @@
 -----------------------------------
 local ID = require("scripts/zones/North_Gustaberg_[S]/IDs")
 require("scripts/globals/mobs")
+require("scripts/globals/zone")
+require("scripts/globals/world")
 -----------------------------------
+
+function onZoneWeatherChange(weather)
+    if weather == tpz.weather.RAIN or weather == tpz.weather.SQUALL then
+        mob:addMod(tpz.mod.REGEN, 10)
+    end
+end
+
+function onGameHour(zone)
+    local hour = VanadielHour()
+    if hour < 4 or hour >= 20 then
+        mob:addMod(tpz.mod.REGAIN, 100)
+    end
+end
 
 function onMobDeath(mob, player, isKiller)
 end
