bebc4bc2deb637643c75a6f7086c21a37a7d7d22
neil mitchell <neil_mitchell89@hotmail.com>
2021-03-25 06:56:19 -0300

    Fixes: #1304 Phial of Osseus Serum

---
+++ scripts/globals/items/phial_of_osseous_serum.lua
@@ -0,0 +1,19 @@
+-----------------------------------------
+-- ID: 21870
+-- Osseous Serum
+-- Enchantment: 60Min, Costume - Skeleton
+-----------------------------------------
+require("scripts/globals/status")
+require("scripts/globals/msg")
+-----------------------------------------
+
+function onItemCheck(target)
+    if not target:canUseMisc(tpz.zoneMisc.COSTUME) then
+        return tpz.msg.basic.CANT_BE_USED_IN_AREA
+    end
+    return 0
+end
+
+function onItemUse(target)
+    target:addStatusEffect(tpz.effect.COSTUME, 564, 0, 3600)
+end
