b17a7e6b8c7b3e409bcb4e2f03bb5a5a20c2d2d8
neil mitchell <neil_mitchell89@hotmail.com>
2021-03-25 22:45:51 -0300

    Fixes: #1302 Moonlight not distributing MP to Party

--- scripts/globals/weaponskills/moonlight.lua
+++ scripts/globals/weaponskills/moonlight.lua
@@ -1,14 +1,16 @@
-------------------------
---    Moonlight     --
-------------------------
+----------------------------------------
+--            Moonlight               --
+----------------------------------------
 require("scripts/globals/status")
 require("scripts/globals/settings")
 require("scripts/globals/weaponskills")
+----------------------------------------
 
 function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local lvl = player:getSkillLevel(11) -- get club skill
     local damage = lvl/9
     local damagemod = damage * (1 + (tp-1000)/1000*1.25)
 	player:trySkillUp(target, tpz.skill.CLUB, 1)
+
     return 1, 0, false, damagemod
 end
