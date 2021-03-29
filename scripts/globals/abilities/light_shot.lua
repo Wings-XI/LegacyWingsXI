d9fd3cccc8d5a66ced425c446a150303f145c645
Lau <8196761-Lau_Cursed@users.noreply.gitlab.com>
2021-03-29 19:52:14 +0000

    Fixes: #1277 Quickdraw factoring in marksmanship skill (should not)

--- scripts/globals/abilities/light_shot.lua
+++ scripts/globals/abilities/light_shot.lua
@@ -22,8 +22,8 @@ end
 
 function onUseAbility(player, target, ability)
     local duration = 60
-    local bonusAcc = (player:getStat(tpz.mod.AGI) - target:getStat(tpz.mod.AGI)) / 2 + player:getMerit(tpz.merit.QUICK_DRAW_ACCURACY) + player:getMod(tpz.mod.QUICK_DRAW_MACC)
-    local resist = applyResistanceAbility(player, target, tpz.magic.ele.LIGHT, tpz.skill.MARKSMANSHIP, bonusAcc)
+    local bonusAcc = (player:getStat(tpz.mod.AGI) - target:getStat(tpz.mod.AGI)) / 2 + player:getMerit(tpz.merit.QUICK_DRAW_ACCURACY) + player:getMod(tpz.mod.QUICK_DRAW_MACC) - 23
+    local resist = applyResistanceAbility(player, target, tpz.magic.ele.LIGHT, tpz.skill.NONE, bonusAcc)
 
     if resist < 0.5 then
         ability:setMsg(tpz.msg.basic.JA_MISS_2) -- resist message
