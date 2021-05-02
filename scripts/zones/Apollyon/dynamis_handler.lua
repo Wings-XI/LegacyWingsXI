1f368286a7b6050905c6fdd1ac695c479efef3b1
Caelic <bclynn96@gmail.com>
2021-05-02 09:26:25 -0400

    add missing files

---
+++ scripts/zones/Apollyon/dynamis_handler.lua
@@ -0,0 +1,45 @@
+-----------------------------------
+--
+-- Apollyon
+-- Limbus
+-----------------------------------
+local ID = require("scripts/zones/Apollyon/IDs")
+require("scripts/globals/dynamis")
+require("scripts/globals/settings")
+require("scripts/globals/status")
+require("scripts/globals/msg")
+require("scripts/globals/utils")
+require("scripts/globals/zone")
+-----------------------------------
+
+function onDynamisTick(timeRemaining)
+    
+    
+    
+end
+
+function onDynamisNewInstance()
+    
+    
+    
+end
+
+function onDynamisCleanup()
+    
+    
+    
+end
+
+function onDynamisEjectPlayer(player, immediate)
+    
+    
+    
+end
+
+function onDynamisTimeWarning(player, timeRemaining)
+    
+    
+    
+end
+
+-- the Zone.lua will handle onZoneIn for the player
---
+++ scripts/zones/Temenos/dynamis_handler.lua
@@ -0,0 +1,43 @@
+-----------------------------------
+--
+-- Temenos
+-- Limbus
+-----------------------------------
+local ID = require("scripts/zones/Temenos/IDs")
+require("scripts/globals/dynamis")
+require("scripts/globals/settings")
+require("scripts/globals/status")
+require("scripts/globals/msg")
+require("scripts/globals/utils")
+require("scripts/globals/zone")
+-----------------------------------
+
+function onDynamisTick(timeRemaining)
+    
+    
+    
+end
+
+function onDynamisNewInstance()
+    
+    
+    
+end
+
+function onDynamisCleanup()
+    
+    
+    
+end
+
+function onDynamisEjectPlayer(player, immediate)
+    
+    
+    
+end
+
+function onDynamisTimeWarning(player, timeRemaining)
+    
+    
+    
+end
