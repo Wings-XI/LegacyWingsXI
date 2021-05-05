1a677fcb7124fa9ac5da821d28fcd39953d3795e
Rdm Kyran <rdmkyran@gmail.com>
2021-05-05 10:47:49 +0000

    Update nm bres

--- scripts/globals/mobskills/dancing_chains.lua
+++ scripts/globals/mobskills/dancing_chains.lua
@@ -9,6 +9,7 @@ require("scripts/globals/status")
 ---------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
+    mob:messageBasic(tpz.msg.basic.READIES_WS, 0, skill:getID())
     return 0
 end
 
