5173c22bfe798b8e29f53ae2380b827fd60ca392
Critical <lyardeen@gmail.com>
2021-05-12 08:37:37 +0000

    Fixes #134: Xolotl overhaul

---
+++ scripts/zones/Attohwa_Chasm/mobs/Xolotls_Hound_Warrior.lua
@@ -0,0 +1,10 @@
+-----------------------------------
+-- Area: Attohwa Chasm
+--  Mob: Xolotl's Hound Warrior
+-- Note: Pet for Xolotl
+-----------------------------------
+local ID = require("scripts/zones/Attohwa_Chasm/IDs")
+-----------------------------------
+
+function onMobDeath(mob, player, isKiller)
+end
