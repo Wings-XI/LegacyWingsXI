deac5da26968c1038a5c961db3f2c6752a966553
Caelic <bclynn96@gmail.com>
2021-05-29 22:51:36 +0000

    draw-in AI improvement

--- scripts/zones/Phomiuna_Aqueducts/mobs/Minotaur.lua
+++ scripts/zones/Phomiuna_Aqueducts/mobs/Minotaur.lua
@@ -4,11 +4,15 @@
 -----------------------------------
 mixins = {require("scripts/mixins/fomor_hate")}
 require("scripts/globals/missions")
+require("scripts/globals/status")
 -----------------------------------
 
 function onMobSpawn(mob)
     mob:setLocalVar("fomorHateDecrease", 1)
     mob:setLocalVar("fomorHateAdj", 2)
+    mob:setMobMod(tpz.mobMod.DRAW_IN, 1)
+    mob:setMobMod(tpz.mobMod.DRAW_IN_INCLUDE_PARTY, 1)
+    mob:setMobMod(tpz.mobMod.DRAW_IN_CUSTOM_RANGE, 15)
 end
 
 function onMobDeath(mob, player, isKiller)
--- src/map/mob_modifier.h
+++ src/map/mob_modifier.h
@@ -40,7 +40,11 @@ enum MOBMODIFIER : int
     MOBMOD_HP_HEAL_CHANCE      = 9,  // can cast cures below this HP %
     MOBMOD_SUBLINK             = 10, // sub link group
     MOBMOD_LINK_RADIUS         = 11, // link radius
-    MOBMOD_DRAW_IN             = 12, // 1 - player draw in, 2 - alliance draw in -- only add as a spawn mod!
+    MOBMOD_DRAW_IN             = 12, // mob will draw-in if its target wanders too far away. can set additional parameters with mobmods 76/77/78/79
+    MOBMOD_DRAW_IN_CUSTOM_RANGE= 76, // override the default range of MeleeRange*2 of when players start to get drawn-in
+    MOBMOD_DRAW_IN_MAXIMUM_REACH=77, // players further than this range (yalms) will be unaffected by the draw-in. default (0) is whole zone
+    MOBMOD_DRAW_IN_INCLUDE_PARTY=78, // this will cause the mob's draw-in to also affect all party and alliance members
+    MOBMOD_DRAW_IN_IGNORE_STATIONARY=79, // stationary mobs draw-in the moment they cannot attack you anymore (out of range). put this mobmod on stationary mobs that have draw-in but use ranged attacks instead of melee attacks so that they will ignore this behavior (i.e. KSNM99 Wyrm or Nyzul Isle Alexander)
     MOBMOD_SEVERE_SPELL_CHANCE = 13, // % chance to use a severe spell like death or impact
     MOBMOD_SKILL_LIST          = 14, // uses given mob skill list
     MOBMOD_MUG_GIL             = 15, // amount gil carried for mugging
@@ -104,6 +108,7 @@ enum MOBMODIFIER : int
     MOBMOD_ATTRACT_FAMILY_NM   = 73, // NMs within the same family will link onto this mob (used on Sabotenders for Cactrot Rapido)
     MOBMOD_FAMILYLINK          = 74, // Mob will link with mobs of the same familly in the zone. Usefull to make a NM link if the family doesn't, like Cactrot Rapido.
     MOBMOD_ENCROACH_PLAYER     = 75,
+    // #76 and #77 and #78 and #79 are taken (placed next to #12 for clarity)
     MOBMOD_SIGHT_ANGLE         = 84, // Angle in which a mob will detect by sight
     MOBMOD_FOMOR_HATE          = 85, // Mob aggros according to fomor hate
     MOBMOD_PIXIE               = 86, // Pixie (heals players)
