00cd446afa14296b2785a5bc6fa7b8075031fc77
Critical <lyardeen@gmail.com>
2021-06-04 13:01:31 -0600

    Ancient Vessel adjustments

--- scripts/zones/Quicksand_Caves/mobs/Ancient_Vessel.lua
+++ scripts/zones/Quicksand_Caves/mobs/Ancient_Vessel.lua
@@ -6,11 +6,17 @@
 require("scripts/globals/missions")
 -----------------------------------
 
-function onMobDeath(mob, player, isKiller)
+function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
+end
 
+function onAdditionalEffect(mob, target, damage)
+    return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.STUN)
+end
+
+function onMobDeath(mob, player, isKiller)
     if (player:getCurrentMission(ZILART) == tpz.mission.id.zilart.THE_MITHRA_AND_THE_CRYSTAL and player:getCharVar("ZilartStatus") == 1) then
         player:needToZone(true)
         player:setCharVar("AncientVesselKilled", 1)
     end
-
 end
