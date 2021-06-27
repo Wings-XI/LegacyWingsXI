69f7e89cc0ad1b5fc8f7e4840888f241f7733d81
kravy <anthony925@comcast.net>
2021-06-27 08:59:46 -0400

    CoP 5-3 L3 A Century of Hardship Mineshaft Fight

---
+++ scripts/zones/Mine_Shaft_2716/mobs/Chekochuk.lua
@@ -0,0 +1,7 @@
+-----------------------------------
+-- Area: Mine Shaft 2716
+-- CoP Mission 5-3 (A Century of Hardship)
+-- NM: Chekochuk
+-----------------------------------
+mixins = {require("scripts/mixins/job_special")}
+-----------------------------------
