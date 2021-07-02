7e2ff138d37f687a763a723158194307a92d5674
Lau <8196761-Lau_Cursed@users.noreply.gitlab.com>
2021-07-02 12:36:46 +0000

    Bugfix #1680 - Reseting Local Vars for Wyrms to prevent stuck logic when wiping

--- scripts/zones/Attohwa_Chasm/mobs/Tiamat.lua
+++ scripts/zones/Attohwa_Chasm/mobs/Tiamat.lua
@@ -8,8 +8,8 @@ mixins = {require("scripts/mixins/families/wyrm_wakeup")}
 -----------------------------------
 
 function onMobSpawn(mob)
-    mob:SetMobSkillAttack(0) -- resetting so it doesn't respawn in flight mode.
-    mob:AnimationSub(0) -- subanim 0 is only used when it spawns until first flight.
+    mob:SetMobSkillAttack(0) -- Resetting so it doesn't respawn in flight mode
+    mob:AnimationSub(0) -- AnimationSub 0 is only used when it spawns until first flight
 
     mob:setMobMod(tpz.mobMod.CLAIM_SHIELD, 1)
     mob:setMobMod(tpz.mobMod.SIGHT_RANGE, 30)
@@ -28,43 +28,45 @@ function onMobSpawn(mob)
 end
 
 function onMobFight(mob, target)
-    -- Gains a large attack boost when health is under 25% which cannot be Dispelled.
-    if (mob:getHP() < ((mob:getMaxHP() / 10) * 2.5)) then
-        if (mob:hasStatusEffect(tpz.effect.ATTACK_BOOST) == false) then
+    -- Gains a large attack boost when health is under 25% which cannot be Dispelled
+    if mob:getHPP() < 25 then
+        if mob:hasStatusEffect(tpz.effect.ATTACK_BOOST) == false then
             mob:addStatusEffect(tpz.effect.ATTACK_BOOST, 75, 0, 0)
             mob:getStatusEffect(tpz.effect.ATTACK_BOOST):setFlag(tpz.effectFlag.DEATH)
         end
+    -- Deletes Effect if regens back up due to a wipe  
+    else
+        if mob:hasStatusEffect(tpz.effect.ATTACK_BOOST) == true then
+            mob:delStatusEffect(tpz.effect.ATTACK_BOOST)
+        end
     end
 
-    if (mob:hasStatusEffect(tpz.effect.MIGHTY_STRIKES) == false and mob:actionQueueEmpty() == true) then
+    if mob:hasStatusEffect(tpz.effect.MIGHTY_STRIKES) == false and mob:actionQueueEmpty() == true then
         local changeTime = mob:getLocalVar("changeTime")
         local twohourTime = mob:getLocalVar("twohourTime")
         local changeHP = mob:getLocalVar("changeHP")
 
-        if (twohourTime == 0) then
+        if twohourTime == 0 then
             twohourTime = math.random(8, 14)
             mob:setLocalVar("twohourTime", twohourTime)
         end
 
-        if (mob:AnimationSub() == 2 and mob:getBattleTime()/15 > twohourTime) then
+        if mob:AnimationSub() == 2 and mob:getBattleTime()/15 > twohourTime then
             mob:useMobAbility(688)
             mob:setLocalVar("twohourTime", math.random((mob:getBattleTime()/15)+4, (mob:getBattleTime()/15)+8))
-        elseif (mob:AnimationSub() == 0 and mob:getBattleTime() - changeTime > 60) then
+        elseif mob:AnimationSub() == 0 and mob:getBattleTime() - changeTime > 60 then
             mob:AnimationSub(1)
             mob:addStatusEffectEx(tpz.effect.TOO_HIGH, 0, 1, 0, 0)
             mob:SetMobSkillAttack(730)
-            --and record the time and HP this phase was started
-            mob:setLocalVar("changeTime", mob:getBattleTime())
+            mob:setLocalVar("changeTime", mob:getBattleTime()) -- Record the time and HP this phase was started
             mob:setLocalVar("changeHP", mob:getHP()/1000)
-        -- subanimation 1 is flight, so check if she should land
-        elseif (mob:AnimationSub() == 1 and (mob:getHP()/1000 <= changeHP - 10 or
-                mob:getBattleTime() - changeTime > 120)) then
+        -- AnimationSub 1 is flight, so check if she should land
+        elseif mob:AnimationSub() == 1 and (mob:getHP()/1000 <= changeHP - 10 or mob:getBattleTime() - changeTime > 120) then
             mob:useMobAbility(1282)
             mob:setLocalVar("changeTime", mob:getBattleTime())
             mob:setLocalVar("changeHP", mob:getHP()/1000)
-        -- subanimation 2 is grounded mode, so check if she should take off
-        elseif (mob:AnimationSub() == 2 and (mob:getHP()/1000 <= changeHP - 10 or
-                mob:getBattleTime() - changeTime > 120)) then
+        -- AnimationSub 2 is grounded mode, so check if she should take off
+        elseif mob:AnimationSub() == 2 and (mob:getHP()/1000 <= changeHP - 10 or mob:getBattleTime() - changeTime > 120) then
             mob:AnimationSub(1)
             mob:addStatusEffectEx(tpz.effect.TOO_HIGH, 0, 1, 0, 0)
             mob:SetMobSkillAttack(730)
@@ -72,6 +74,13 @@ function onMobFight(mob, target)
             mob:setLocalVar("changeHP", mob:getHP()/1000)
         end
     end
+end
+
+-- Prevents any stuck logic due to wipes
+function onMobDisengage(mob)
+    mob:setLocalVar("changeTime", 0)
+    mob:setLocalVar("twohourTime", 0)
+    mob:setLocalVar("changeHP", 0)   
 end
 
 function onCastStarting(mob, spell)
