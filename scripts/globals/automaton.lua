91a7233ddb42bc051413a818024d0d3a91252f8a
Tiberon <TiberonKalkaz@gmail.com>
2021-07-21 14:25:38 -0700

    Adds bonus stats to puppet from maneuvers

--- scripts/globals/automaton.lua
+++ scripts/globals/automaton.lua
@@ -1,3 +1,9 @@
+----------------------------------
+-- Global Automaton Helpers
+----------------------------------
+require("scripts/globals/status")
+----------------------------------
+
 function updateModPerformance(pet, mod, key, value, cap)
     local previous = pet:getLocalVar(key)
     if previous ~= 0 then
@@ -9,4 +15,33 @@ function updateModPerformance(pet, mod, key, value, cap)
     end
     pet:addMod(mod, value)
     pet:setLocalVar(key, value)
+end
+
+
+-- Determines the stat bonus an Automaton shold recieve per maneuver
+function getManeuverBonus(player)
+    local level
+    if (player:getMainJob() == tpz.job.PUP) then
+        level = player:getMainLvl()
+    else
+        level = player:getSubLvl()
+    end
+
+    local bonus = 0
+
+    if level <= 14 then
+        bonus = 1
+    elseif level <= 29 then
+        bonus = 2
+    elseif level <= 44 then
+        bonus = 3
+    elseif level <= 59 then
+        bonus = 4
+    elseif level <= 74 then
+        bonus = 5
+    else
+        bonus = 6
+    end
+
+    return bonus
 end
