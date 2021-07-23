770af8beb4aacbf2f4410cd12e2e9ea06346f726
Caelic <bclynn96@gmail.com>
2021-07-23 05:30:02 -0400

    cleanup of more unused dynamis vars

--- scripts/zones/Dynamis-Xarcabard/dynamis_handler.lua
+++ scripts/zones/Dynamis-Xarcabard/dynamis_handler.lua
@@ -14,7 +14,6 @@ require("scripts/globals/zone")
 -----------------------------------
 
 local zone = tpz.zone.DYNAMIS_XARCABARD
-nmsKilled = 0
 
 function onDynamisTick(timeRemaining)
     -- haven't had a reason to use this yet, for now this function is disabled for optimization purposes (a lua callback every server tick!!)
@@ -32,7 +31,6 @@ function onDynamisNewInstance()
     if npcList == nil then print("npcList was nil") end
     if npcList[zone] == nil then print("npcList[zone] was nil") end
     
-    nmsKilled = 0
     while i <= iEnd do
         entity = GetEntityByID(i)
         if entity ~= nil and entity:isNPC() then entity:setStatus(tpz.status.DISAPPEAR) end
