fa13ad41731f429db098bcd90d58b8217462a0a2
kravy <anthony925@comcast.net>
2021-07-29 09:07:55 -0400

    Reduce Bugbby and his moblins damage, add HP threshold to bugbby mighty strikes.

--- scripts/zones/Mine_Shaft_2716/mobs/Movamuq.lua
+++ scripts/zones/Mine_Shaft_2716/mobs/Movamuq.lua
@@ -6,5 +6,9 @@
 mixins = {require("scripts/mixins/job_special")}
 -----------------------------------
 
+function onMobInitialize(mob)
+    mob:setMod(tpz.mod.ATTP, -75)
+end
+
 function onMobDeath(mob, player, isKiller)
 end
