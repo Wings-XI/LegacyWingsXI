607ecc26943dda48fc3bec8b7219cbf3e811ecea
Twilight <23407689+NiQ1@users.noreply.github.com>
2021-07-29 19:38:23 +0300

    Fix apprisal NPC giving free apprisals.

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Drahbah.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Drahbah.lua
@@ -3,6 +3,7 @@
 --  NPC: Drahbah
 -- Standard Info NPC
 -----------------------------------
+local ID = require("scripts/zones/Aht_Urhgan_Whitegate/IDs")
 require("scripts/globals/appraisal")
 require("scripts/globals/npc_util")
 
@@ -10,7 +11,7 @@ local gilToPay = 500
 
 function onTrade(player, npc, trade)
     local appraisalCsid = 679
-    tpz.appraisalUtil.appraiseItem(player, npc, trade, gilToPay, appraisalCsid)
+    tpz.appraisalUtil.appraiseItem(player, npc, trade, gilToPay, appraisalCsid, { ID.text.NOT_HAVE_ENOUGH_GIL, 4618 })
 end
 
 function onTrigger(player, npc)
