abdc7349895e8b603e21c622fd99a36eb940ce03
Booma <6395076-gigobooma@users.noreply.gitlab.com>
2021-08-01 11:33:16 +0100

    Removed variable minimum respawn from Steam Cleaner

--- scripts/zones/VeLugannon_Palace/mobs/Steam_Cleaner.lua
+++ scripts/zones/VeLugannon_Palace/mobs/Steam_Cleaner.lua
@@ -15,7 +15,6 @@ function onMobDeath(mob, player, isKiller)
 end
 
 function onMobDespawn(mob)
-    local respawn = math.random(7200, 14400)
-    mob:setRespawnTime(respawn) -- 2 to 4 hours
+    mob:setRespawnTime(7200) -- 2 hours
     SetServerVariable("SteamCleaner_Respawn", (os.time() + respawn))
 end
