cfa4219b4454a3fb61a41ec0cf309e2b0b8a9153
Caelic <bclynn96@gmail.com>
2021-08-08 01:42:04 +0000

    fix mobs not despawning when they roam out of range of their spawn

--- src/map/ai/controllers/mob_controller.cpp
+++ src/map/ai/controllers/mob_controller.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 ===========================================================================
 
 Copyright (c) 2010-2015 Darkstar Dev Teams
@@ -879,7 +879,7 @@ void CMobController::DoRoamTick(time_point tick)
                     // move back every 5 seconds
                     m_LastActionTime = m_Tick - (std::chrono::milliseconds(PMob->getBigMobMod(MOBMOD_ROAM_COOL)) + 10s);
                 }
-                else if (PMob->getMobMod(MOBMOD_NO_DESPAWN) && !map_config.mob_no_despawn)
+                else if (!PMob->getMobMod(MOBMOD_NO_DESPAWN) && !map_config.mob_no_despawn)
                 {
                     PMob->PAI->Despawn();
                     return;
