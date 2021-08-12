458a467a2b8660497730858ea47a7241504a6a77
demosth <demosthene84@gmail.com>
2021-08-12 03:42:30 +0000

    fix(assaults): Multiple fix for Excavation Duty (rank 1 assault)

--- scripts/zones/Lebros_Cavern/mobs/Qiqirn_Mine.lua
+++ scripts/zones/Lebros_Cavern/mobs/Qiqirn_Mine.lua
@@ -12,7 +12,6 @@ require("scripts/globals/instance")
 function onMobSpawn(mob)
     local instance = mob:getInstance()
     local players = instance:getChars()
-    mob:hideName(true)
     mob:setMobMod(tpz.mobMod.NO_MOVE, 1)
     mob:SetAutoAttackEnabled(false)
 
@@ -22,27 +21,30 @@ function onMobSpawn(mob)
         mob:timer(6000, function(mob) v:messageSpecial(ID.text.MINE_COUNTDOWN, 4) end)
         mob:timer(7000, function(mob) v:messageSpecial(ID.text.MINE_COUNTDOWN, 3) end)
         mob:timer(8000, function(mob) v:messageSpecial(ID.text.MINE_COUNTDOWN, 2) end)
-        mob:timer(9000, function(mob) v:messageSpecial(ID.text.MINE_COUNTDOWN, 1) mob:useMobAbility(1838) end)
-        mob:timer(11000, function(mob) mob:setHP(0) end)
+        mob:timer(9000, function(mob) v:messageSpecial(ID.text.MINE_COUNTDOWN, 1) end)
+        mob:timer(10000, function(mob) v:messageSpecial(ID.text.MINE_COUNTDOWN, 0) end)
+        mob:timer(10000, function(mob) 
+            if mob:getTarget():getTarget() ~= nil then
+                mob:useMobAbility(1838) 
+            end
+        end)
+        mob:timer(12000, function(mob) mob:setHP(0) end)
     end
 end
 
-function onMobWeaponSkill(target, mob, skill)
+function onMobSkillFinished(mob, target, skill)
     if target:getTarget() ~= nil then
+        local skillId = skill:getID()
         local wallid = target:getTarget():getID()
         local instance = target:getInstance()
         local wall = instance:getEntity(bit.band(wallid, 0xFFF), tpz.objType.MOB)
-        wall:triggerListener("WEAPONSKILL_TAKE", wall, skill:getID())
+        wall:timer(2000, function(wall) wall:triggerListener("WEAPONSKILL_TAKE", wall, skillId) end)
+        return wall:getShortID()
     end
+    return 0
 end
 
 function onMobFight(mob, target)
-    -- local wallid = target:getTarget():getID()
-    -- local instance = target:getInstance()
-    -- local wall = instance:getEntity(bit.band(wallid, 0xFFF), tpz.objType.MOB)
-    -- if bomb_timer == 1 then
-    --     wall:triggerListener("WEAPONSKILL_TAKE", wall, 1838) 
-    -- end
 end
 
 function onMobDeath(mob, player, isKiller)
