b1c935764282e25102423238b87ab098fa03fcbd
Caelic <bclynn96@gmail.com>
2021-08-13 15:24:23 -0400

    dyna sandy hecteyes sleep immunity

--- scripts/zones/Dynamis-San_dOria/mobs/Vanguards_Hecteyes.lua
+++ scripts/zones/Dynamis-San_dOria/mobs/Vanguards_Hecteyes.lua
@@ -3,8 +3,11 @@
 --  Mob: Vanguard's Hecteyes
 -----------------------------------
 require("scripts/globals/dynamis")
+require("scripts/globals/status")
 -----------------------------------
 
 function onMobSpawn(mob)
     dynamis.setPetStats(mob)
+    mob:setMod(tpz.mod.SLEEPRESTRAIT, 100)
+    mob:setMod(tpz.mod.LULLABYRESTRAIT, 100)
 end
