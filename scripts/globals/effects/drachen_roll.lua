660b60a9fd75fb4ecb1439be945219e21fab7690
DESKTOP-DO5L1TR\cslou <csloughlin@gmail.com>
2021-08-15 02:21:21 -0400

    Drachen Roll Corrected was giving acc to the pet not macc and matt

--- scripts/globals/effects/drachen_roll.lua
+++ scripts/globals/effects/drachen_roll.lua
@@ -7,7 +7,8 @@
 require("scripts/globals/status")
 
 function onEffectGain(target, effect)
-    target:addPetMod(tpz.mod.ACC, effect:getPower())
+    target:addPetMod(tpz.mod.MACC, effect:getPower())
+    target:addPetMod(tpx.mod.MATT, effect:getPower())
 end
 
 function onEffectTick(target, effect)
@@ -15,4 +16,5 @@ end
 
 function onEffectLose(target, effect)
     target:delPetMod(tpz.mod.ACC, effect:getPower())
+    target:delPetMod(tpx.mod.MATT, effect:getPower())
 end
