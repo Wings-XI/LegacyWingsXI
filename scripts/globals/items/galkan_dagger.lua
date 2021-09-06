a096324bf8125b29f6b8b2428ca9d47dc96712e7
Tiberon <TiberonKalkaz@gmail.com>
2021-09-06 10:41:06 -0700

    Adds latent effect to galkan dagger

---
+++ scripts/globals/items/galkan_dagger.lua
@@ -0,0 +1,35 @@
+-----------------------------------------
+-- ID: 19114
+-- Item: Galkan Dagger
+-- Additional Effect: Earth Damage
+-----------------------------------------
+require("scripts/globals/status")
+require("scripts/globals/magic")
+require("scripts/globals/msg")
+-----------------------------------
+
+function onAdditionalEffect(player,target,damage)
+    local chance = 50
+
+    if (math.random(0,99) >= chance) then
+        printf("NO\n")
+        return 0,0,0
+    else
+        printf("YES\n")
+        local dmg = math.random(14,16)
+        local params = {}
+        params.bonusmab = 0
+        params.includemab = false
+        dmg = addBonusesAbility(player, tpz.magic.ele.EARTH, target, dmg, params)
+        dmg = dmg * applyResistanceAddEffect(player,target,tpz.magic.ele.EARTH,0)
+        dmg = adjustForTarget(target,dmg,tpz.magic.ele.EARTH)
+        dmg = finalMagicNonSpellAdjustments(player,target,tpz.magic.ele.EARTH,dmg)
+
+        local message = tpz.msg.basic.ADD_EFFECT_DMG
+        if (dmg < 0) then
+            message = tpz.msg.basic.ADD_EFFECT_HEAL
+        end
+
+        return tpz.subEffect.EARTH_DAMAGE,message,dmg
+    end
+end
