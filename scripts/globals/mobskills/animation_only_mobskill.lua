c1ce2ad8ff37dc5cf4b5edcb93afa9d61eab10e8
Tiberon <TiberonKalkaz@gmail.com>
2021-09-07 10:54:46 -0700

    Adds documentation on animation only mobskills. Updates Valkengs_Dance to use animation only mobskill

--- scripts/globals/mobskills/animation_only_mobskill.lua
+++ scripts/globals/mobskills/animation_only_mobskill.lua
@@ -1,6 +1,13 @@
 ---------------------------------------------
 -- Animation Only Mobskill
--- Dummy Skill that triggers an animation.
+-- This script is a catch all for skills which only trigger an animation when used by a mob.
+--  Known skills which follow this pattern:
+--  SkillID  AnimationID NameAppearingOnCapture  MobUsingSkill  Effect
+--  307      439         Substitute              Galarhigg      2 Hour "dust cloud" - entity flashes black
+--  603      432         Lateral_Slash           Galarhigg      2 Hour "dust cloud" - entity flashes blue
+--  624      434         Vulture_3               Galarhigg      2 Hour "dust cloud" - entity flashes green
+--  626      437         Vulture_1               Galarhigg      2 Hour "dust cloud" - entity flashes white
+--  2018     1365        Unknown                 Valkeng        Puppet Frame - Performs a dance then hides the mob model
 ---------------------------------------------
 require("scripts/globals/monstertpmoves")
 require("scripts/globals/settings")
@@ -13,7 +20,6 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-
     skill:setMsg(tpz.msg.basic.NONE)
 
     return 0
