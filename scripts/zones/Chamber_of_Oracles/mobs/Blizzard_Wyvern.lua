479f8983e5a1cd9f62553de94cf6ede159574eb2
DESKTOP-4P06VF9\alexs <admin@mageringnetwork.com>
2021-09-07 01:10:37 -0400

    Eye of the Storm KSNM30.

--- scripts/zones/Chamber_of_Oracles/mobs/Blizzard_Wyvern.lua
+++ scripts/zones/Chamber_of_Oracles/mobs/Blizzard_Wyvern.lua
@@ -1,12 +1,13 @@
 -----------------------------------
 -- Area: Chamber of Oracles
---  Mob: Blizzard Wyvern
+--  Mob: Radiant Wyvern
 -- KSNM: Eye of the Storm
 -----------------------------------
 require("scripts/globals/status")
 -----------------------------------
 function onMobSpawn(mob)
     mob:setMobMod(tpz.mobMod.SIGHT_RANGE, 17)
+	mob:setMod(tpz.mod.SLEEPRES, 256) -- Will not sleep without Elemental Seal per wiki.
 end
 
 function onMobEngaged(mob, target)
--- scripts/zones/Chamber_of_Oracles/mobs/Chaos_Wyvern.lua
+++ scripts/zones/Chamber_of_Oracles/mobs/Chaos_Wyvern.lua
@@ -1,14 +1,14 @@
 -----------------------------------
 -- Area: Chamber of Oracles
---  Mob: Chaos Wyvern
+--  Mob: Radiant Wyvern
 -- KSNM: Eye of the Storm
 -----------------------------------
 require("scripts/globals/status")
 -----------------------------------
 function onMobSpawn(mob)
     mob:setMobMod(tpz.mobMod.SIGHT_RANGE, 17)
+	mob:setMod(tpz.mod.SLEEPRES, 256) -- Will not sleep without Elemental Seal per wiki.
 end
-
 
 function onMobEngaged(mob, target)
     mob:useMobAbility(815)
