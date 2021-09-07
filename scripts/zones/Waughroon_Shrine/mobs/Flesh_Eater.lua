690e3de5611531d42b566796135cb15267b1afca
Gweivyth Quartet <admin@mageringnetwork.com>
2021-09-07 04:43:08 +0000

    Update scripts/zones/Waughroon_Shrine/mobs/Flesh_Eater.lua

--- scripts/zones/Waughroon_Shrine/mobs/Flesh_Eater.lua
+++ scripts/zones/Waughroon_Shrine/mobs/Flesh_Eater.lua
@@ -10,5 +10,22 @@ function onMobSpawn(mob)
     mob:setMobMod(tpz.mobMod.CHARMABLE, 1)
 end
 
+function onMonsterMagicPrepare(mob, target)
+    local rnd = math.random()
+
+    if rnd < 0.4
+	then
+    return 190 -- Stonega II
+    elseif rnd < 0.7 then
+        return 189 -- Stonega
+    elseif rnd < 0.8 then
+        return 238 -- Rasp
+	elseif rnd < 0.9 then
+		return 258 -- Bind
+    else
+        return 159 -- Stone
+    end
+end
+
 function onMobDeath(mob, player, isKiller)
 end
