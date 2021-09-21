b9b1f57536f594928cec14b80371bef315fe793f
Kyuu <delomprep@gmail.com>
2021-09-21 09:51:49 +0200

    rebase, again

--- scripts/zones/PsoXja/mobs/Gargoyle-Lambda.lua
+++ scripts/zones/PsoXja/mobs/Gargoyle-Lambda.lua
@@ -23,12 +23,12 @@ function onMobFight(mob, target)
             mob:delStatusEffectSilent(tpz.effect.PHYSICAL_SHIELD)
             mob:addStatusEffect(tpz.effect.MAGIC_SHIELD, 1, 0, 0)
             mob:useMobAbility(604)
-            mob:setSpellList(476)
+            mob:setSpellList(498)
         else
             mob:delStatusEffectSilent(tpz.effect.MAGIC_SHIELD)
             mob:addStatusEffect(tpz.effect.PHYSICAL_SHIELD, 1, 0, 0)
             mob:useMobAbility(603)
-            mob:setSpellList(477)
+            mob:setSpellList(499)
         end
         mob:setLocalVar("switchTime", mob:getBattleTime())
     end
