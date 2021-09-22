6557af4a9d50fab5022b87e3cf1e8b5ef1f26195
DESKTOP-976J67T\Connor <kubiakconnor@gmail.com>
2021-09-22 17:54:58 -0400

    added easier-to-read en-spell proc chance

--- scripts/zones/Grauberg_[S]/mobs/Scitalis.lua
+++ scripts/zones/Grauberg_[S]/mobs/Scitalis.lua
@@ -8,16 +8,20 @@ require("scripts/globals/status")
 require("scripts/globals/mobs")
 ------------------------------
 
-function onMobSpawn(mob, player, isKiller)
-    mob:setMod(tpz.mod.DOUBLE_ATTACK, 50)
-    mob:addMod(tpz.mod.WIND_ABSORB, 100)
-    mob:addMod(tpz.mod.SLEEPRES, 95)
+function onMobInitialize(mob)
     mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
 end
 
-function onAdditionalEffect(mob, target, damage)
-    return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.ENAERO, {power = math.random(120, 130), chance = 100})
+function onMobSpawn(mob)
+    mob:setMod(tpz.mod.DOUBLE_ATTACK, 50)
+    mob:addMod(tpz.mod.WIND_ABSORB, 100)
+    mob:addMod(tpz.mod.SLEEPRES, 95)
 end
+
+
+function onAdditionalEffect(mob, target, damage)
+    return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.ENAERO, {power = math.random(120,140)})
+ end
 
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 503)
