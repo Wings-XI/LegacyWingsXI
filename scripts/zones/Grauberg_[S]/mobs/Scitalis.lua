dfcf1e1617bb39e1786e618cc9bdea66adb17e55
DESKTOP-976J67T\Connor <kubiakconnor@gmail.com>
2021-09-21 22:06:13 -0400

    NM "Scitalis" in Grauberg (S) audit

--- scripts/zones/Grauberg_[S]/mobs/Scitalis.lua
+++ scripts/zones/Grauberg_[S]/mobs/Scitalis.lua
@@ -1,9 +1,22 @@
 ------------------------------
 -- Area: Grauberg [S]
 --   NM: Scitalis
+-- mob ID: 17141979
 ------------------------------
 require("scripts/globals/hunts")
+require("scripts/globals/status")
 ------------------------------
+
+function onMobSpawn(mob, player, isKiller)
+    mob:setMod(tpz.mod.DOUBLE_ATTACK, 50)
+    mob:addMod(tpz.mod.WIND_ABSORB, 100)
+    mob:addMod(tpz.mod.SLEEPRES, 95)
+    mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
+end
+
+function onAdditionalEffect(mob, target, damage)
+    return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.ENAERO, {power = math.random(120, 130), chance = 1000})
+end
 
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 503)
