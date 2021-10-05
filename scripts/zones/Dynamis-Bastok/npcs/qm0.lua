eef07170769265a488b935a2c3da7a4bf3fbd2ef
Caelic <bclynn96@gmail.com>
2021-10-05 06:15:03 -0400

    dynamis win ??? rework to workaround wrong key item rewards

--- scripts/zones/Dynamis-Bastok/npcs/qm0.lua
+++ scripts/zones/Dynamis-Bastok/npcs/qm0.lua
@@ -8,9 +8,23 @@ require("scripts/globals/keyitems")
 -----------------------------------
 
 function onTrigger(player, npc)
-    if player:hasKeyItem(tpz.ki.HYDRA_CORPS_EYEGLASS) == false then
-        player:addKeyItem(tpz.ki.HYDRA_CORPS_EYEGLASS)
-        local ID = require("scripts/zones/Dynamis-Bastok/IDs")
-        player:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.HYDRA_CORPS_EYEGLASS)
+    local currTime = os.time()
+    if npc:getLocalVar("lastActivation") + 5 > currTime then return end
+    npc:setLocalVar("lastActivation", currTime)
+    
+    -- to fix issues with these QMs bugging out and giving wrong key items/deleting key items/not giving out key items
+    -- they can only be activated every 5 seconds and will grant EVERYONE in range the key item
+    -- because the issue is rooted in a big mob of people spam-activating the NPC at the same time
+    -- a proper fix will require a full re-write of the cs/event system in core (much like Eden did)
+    
+    local nearbyPlayers = npc:getPlayersInRange(50)
+    if nearbyPlayers == nil then return end
+    local ID = require("scripts/zones/Dynamis-Bastok/IDs")
+    
+    for _,v in ipairs(nearbyPlayers) do
+        if v:hasKeyItem(tpz.ki.HYDRA_CORPS_EYEGLASS) == false then
+            v:addKeyItem(tpz.ki.HYDRA_CORPS_EYEGLASS)
+            v:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.HYDRA_CORPS_EYEGLASS)
+        end
     end
 end
