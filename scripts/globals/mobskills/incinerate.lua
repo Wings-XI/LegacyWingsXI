3621ddea01e77f530aae6fcc8d061a4a2c2d2f36
Nireya <aesh@protonmail.com>
2021-10-23 00:13:45 +0000

    Add 800 damage cap to Incinerate

--- scripts/globals/mobskills/incinerate.lua
+++ scripts/globals/mobskills/incinerate.lua
@@ -12,7 +12,7 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = MobBreathMove(mob, target, 0.25, 0.75, tpz.magic.ele.FIRE)
+    local dmgmod = MobBreathMove(mob, target, 0.25, 0.75, tpz.magic.ele.FIRE, 800)
     local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.BREATH, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
 
     target:takeDamage(dmg, mob, tpz.attackType.BREATH, tpz.damageType.FIRE)
