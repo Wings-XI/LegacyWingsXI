2bab95bf5bc90b245de9d1946ab3b34ea1574bec
Velyn <9047311-Mehvvy@users.noreply.gitlab.com>
2021-11-10 20:14:16 +0000

    Add Yell GM commands

---
+++ scripts/commands/yellban.lua
@@ -0,0 +1,43 @@
+-----------------------------------------------------------------------
+-- func: yellban
+-- desc:
+-----------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 1,
+    parameters = "s"
+}
+
+function onTrigger(player, name)
+    if name == nil then
+        player:PrintToPlayer("!yellban: You must provide a player name.")
+        player:PrintToPlayer("!yellban: usage: !yellban <player>")
+        return
+    end
+
+    local infractions = GetCharVarByName(name, "YellInfractions")
+
+    if infractions == nil then
+        player:PrintToPlayer(string.format("!yellban: Player \"%s\" not found!", name))
+        return
+    end
+
+    -- Increase infraction count by one.
+    infractions = infractions + 1
+    SetCharVarByName(name, "YellInfractions", infractions)
+
+    -- Mute the player for a very, very long time.
+    local muteTime = os.time() + 10 * 366 * 86400;
+    local result = SetCharVarByName(name, "YellMuteTime", muteTime)
+    if result == nil then
+        player:PrintToPlayer(string.format("!yellban: Failed to yell mute \"%s\"!", name))
+        return
+    end
+
+    -- reset their rules state
+    SetCharVarByName(name, "YellReadRules", 0)
+    SetCharVarByName(name, "YellOptedIn", 0)
+
+    player:PrintToPlayer(string.format("\"%s\" is muted from yell until %s.", name, os.date("%c", muteTime)), 29)
+end
--- src/map/lua/luautils.cpp
+++ src/map/lua/luautils.cpp
@@ -179,6 +179,9 @@ namespace luautils
         lua_register(LuaHandle, "GetTickets", luautils::GetTickets);
         lua_register(LuaHandle, "GetDynaTimeRemaining", luautils::GetDynaTimeRemaining);
 
+        lua_register(LuaHandle, "GetCharVarByName", luautils::GetCharVarByName);
+        lua_register(LuaHandle, "SetCharVarByName", luautils::SetCharVarByName);
+
         Lunar<CLuaAbility>::Register(LuaHandle);
         Lunar<CLuaAction>::Register(LuaHandle);
         Lunar<CLuaBaseEntity>::Register(LuaHandle);
@@ -5647,6 +5650,72 @@ namespace luautils
         else
             lua_pushinteger(LuaHandle, 0);
 
+        return 1;
+    }
+
+    int32 GetCharVarByName(lua_State* L)
+    {
+        bool isValid = !lua_isnil(L, 1) && lua_isstring(L, 1);
+        isValid = isValid && !lua_isnil(L, 2) && lua_isstring(L, 2);
+
+        if (isValid)
+        {
+            const char* fmtQuery = "SELECT charid FROM chars WHERE charname = '%s';";
+            int32 ret = Sql_Query(SqlHandle, fmtQuery, std::string(lua_tostring(L, 1)).c_str());
+
+            if (ret != SQL_ERROR && Sql_NumRows(SqlHandle) != 0 && Sql_NextRow(SqlHandle) == SQL_SUCCESS)
+            {
+                const char* fmtQuery = "SELECT value FROM char_vars WHERE charid = %d AND varname = '%s';";
+                auto charid = Sql_GetUIntData(SqlHandle, 0);
+
+                int32 ret = Sql_Query(SqlHandle, fmtQuery, charid, std::string(lua_tostring(L, 2)).c_str());
+                if (ret != SQL_ERROR && Sql_NumRows(SqlHandle) != 0 && Sql_NextRow(SqlHandle) == SQL_SUCCESS)
+                {
+                    int32 value = Sql_GetIntData(SqlHandle, 0);
+                    lua_pushinteger(LuaHandle, value);
+                    return 1;
+                }
+
+                lua_pushinteger(LuaHandle, 0);
+                return 1;
+            }
+        }
+
+        lua_pushnil(L);
+        return 1;
+    }
+
+    int32 SetCharVarByName(lua_State* L)
+    {
+        bool isValid = !lua_isnil(L, 1) && lua_isstring(L, 1);
+        isValid = isValid && !lua_isnil(L, 2) && lua_isstring(L, 2);
+        isValid = isValid && !lua_isnil(L, 2) && lua_isnumber(L, 3);
+
+        if (isValid)
+        {
+            const char* fmtQuery = "SELECT charid FROM chars WHERE charname = '%s';";
+            int32 ret = Sql_Query(SqlHandle, fmtQuery, std::string(lua_tostring(L, 1)).c_str());
+
+            if (ret != SQL_ERROR && Sql_NumRows(SqlHandle) != 0 && Sql_NextRow(SqlHandle) == SQL_SUCCESS)
+            {
+                auto value = (int32) lua_tointeger(L, 3);
+
+                const char* fmtQuery = value != 0
+                    ? "INSERT INTO char_vars (charid, varname, value) VALUES (%d, '%s', %d) ON DUPLICATE KEY UPDATE value = VALUES(value);"
+                    : "DELETE FROM char_vars WHERE charid = %d AND varname = '%s';";
+
+                auto charid = Sql_GetUIntData(SqlHandle, 0);
+
+                int32 ret = Sql_Query(SqlHandle, fmtQuery, charid, std::string(lua_tostring(L, 2)).c_str(), value);
+                if (ret != SQL_ERROR && Sql_AffectedRows(SqlHandle) != 0)
+                {
+                    lua_pushinteger(LuaHandle, value);
+                    return 1;
+                }
+            }
+        }
+
+        lua_pushnil(L);
         return 1;
     }
 
