c3e5ace5791b7e5aaa2334593091eb82b61fecb2
Ari <gregm@osmstudios.com>
2021-11-17 18:06:14 -0700

    Change immunities

--- scripts/zones/QuBia_Arena/mobs/Seed_Yagudo.lua
+++ scripts/zones/QuBia_Arena/mobs/Seed_Yagudo.lua
@@ -8,7 +8,7 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onMobSpawn(mob)
-    mob:addMod(tpz.mod.LULLABYRES, 80)
+    mob:addMod(tpz.mod.SLEEPRESTRAIT, 100)
     mob:setMobMod(tpz.mobMod.NO_DROPS, 1)
 end
 
