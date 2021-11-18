7483503dfd1eac8076058dca9f60e4ed08fddabc
Critical <lyardeen@gmail.com>
2021-11-18 15:04:15 -0700

    Stray Mary can no longer spawn twice and respawn timer corrected

--- scripts/zones/Konschtat_Highlands/mobs/Stray_Mary.lua
+++ scripts/zones/Konschtat_Highlands/mobs/Stray_Mary.lua
@@ -2,13 +2,29 @@
 -- Area: Konschtat Highlands
 --   NM: Stray Mary
 -----------------------------------
+local ID = require("scripts/zones/Konschtat_Highlands/IDs")
 require("scripts/globals/hunts")
 require("scripts/globals/titles")
 require("scripts/quests/tutorial")
 -----------------------------------
 
+function onMobSpawn(mob)
+    -- Disallow two Stray Marys from spawning
+    if mob:getID() == ID.Konschtat_Highlands.STRAY_MARY_N then
+        DisallowRespawn(ID.Konschtat_Highlands.STRAY_MARY_S, true)
+    elseif mob:getID() == ID.Konschtat_Highlands.STRAY_MARY_S
+        DisallowRespawn(ID.Konschtat_Highlands.STRAY_MARY_N, true)
+    end
+end
+
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 203)
     player:addTitle(tpz.title.MARYS_GUIDE)
     tpz.tutorial.onMobDeath(player)
+
+    if mob:getID() == ID.Konschtat_Highlands.STRAY_MARY_N then
+        DisallowRespawn(ID.Konschtat_Highlands.STRAY_MARY_S, false)
+    elseif mob:getID() == ID.Konschtat_Highlands.STRAY_MARY_S
+        DisallowRespawn(ID.Konschtat_Highlands.STRAY_MARY_N, false)
+    end
 end
