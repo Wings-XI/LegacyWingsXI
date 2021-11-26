07a2b5aaa2dbc75367831a1e00037cbf7562c292
Ophelia <heliashelium@gmail.com>
2021-11-26 13:39:30 +0000

    Jeuno NPC Movement Add and NPC Look Cleanup

--- scripts/zones/Lower_Jeuno/npcs/Nakho_Jawantal.lua
+++ scripts/zones/Lower_Jeuno/npcs/Nakho_Jawantal.lua
@@ -3,6 +3,42 @@
 --  NPC: Jawantal
 -- Standard Info NPC
 -----------------------------------
+require("scripts/globals/pathfind")
+-----------------------------------
+
+local path =
+{
+-64, -6, -130,
+-65, -6, -130,
+-65, -6, -130,
+-65, -6, -130,
+-65, -6, -130,
+-65, -6, -130,
+-65, -6, -130,
+-65, -6, -130,
+-65, -6, -130,
+-65, -6, -130,
+-67, -6, -133,
+-68, -6, -133,
+-68, -6, -133,
+-68, -6, -133,
+-68, -6, -133,
+-68, -6, -133,
+-68, -6, -133,
+-68, -6, -133,
+-68, -6, -133,
+-68, -6, -133,
+}
+
+function onSpawn(npc)
+    npc:initNpcAi()
+    npc:setPos(tpz.path.first(path))
+    onPath(npc)
+end
+
+function onPath(npc)
+    tpz.path.patrolsimple(npc, path)
+end
 
 function onTrade(player, npc, trade)
 end
--- scripts/zones/RuLude_Gardens/npcs/Leis.lua
+++ scripts/zones/RuLude_Gardens/npcs/Leis.lua
@@ -4,7 +4,55 @@
 -- Standard Info NPC
 -----------------------------------
 require("scripts/globals/settings")
+require("scripts/globals/pathfind")
 -----------------------------------
+
+local path =
+{
+-13, 2, 137,
+-13, 2, 137,
+-13, 2, 137,
+-13, 2, 137,
+-13, 2, 137,
+-13, 2, 137,
+-13, 2, 137,
+-13, 2, 137,
+-13, 2, 137,
+-8, 2, 137,
+-8, 2, 137,
+-8, 2, 137,
+-8, 2, 137,
+-8, 2, 137,
+-8, 2, 137,
+-9, 2, 134,
+-8, 2, 134,
+-8, 2, 134,
+-8, 2, 134,
+-8, 2, 134,
+-8, 2, 134,
+-8, 2, 134,
+-8, 2, 134,
+-8, 2, 132,
+-8, 2, 132,
+-8, 2, 132,
+-8, 2, 132,
+-8, 2, 132,
+-8, 2, 132,
+-8, 2, 132,
+-8, 2, 132,
+-8, 2, 132,
+-10, 2, 137,
+}
+
+function onSpawn(npc)
+    npc:initNpcAi()
+    npc:setPos(tpz.path.first(path))
+    onPath(npc)
+end
+
+function onPath(npc)
+    tpz.path.patrolsimple(npc, path)
+end
 
 function onTrade(player, npc, trade)
 end
