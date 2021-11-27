4b0fa8b01ddf6e01918eb1092ee11f327da60539
OPHELIA-PC\helia <heliashelium@gmail.com>
2021-11-27 04:50:43 -0600

    Pathfind Function Upgrade

--- src/map/lua/lua_baseentity.cpp
+++ src/map/lua/lua_baseentity.cpp
@@ -1916,7 +1916,14 @@ inline int32 CLuaBaseEntity::pathThrough(lua_State* L)
         lua_rawgeti(L, 1, i + 1);
         lua_rawgeti(L, 1, i + 2);
 
-        points.push_back({(float)lua_tointeger(L, -3), (float)lua_tointeger(L, -2), (float)lua_tointeger(L, -1), 0, 0});
+        if (lua_isnil(L, -1) || lua_isnil(L, -2) || lua_isnil(L, -3)) 
+        {
+            //error exit
+            ShowError("Lua::pathThrough : Path value is nil.");
+            return 0;
+        }
+
+        points.push_back({(float)lua_tonumber(L, -3), (float)lua_tonumber(L, -2), (float)lua_tonumber(L, -1), 0, 0});
 
         lua_pop(L, 3);
     }
