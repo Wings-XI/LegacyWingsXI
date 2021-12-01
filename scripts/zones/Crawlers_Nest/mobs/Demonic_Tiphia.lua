47d651414bd29788a939377126e9418565bbda9c
Critical <lyardeen@gmail.com>
2021-12-01 14:56:12 -0700

    Demonic Tiphia HP/MP adjusted, behavior, lottery, and PH corrected

--- scripts/zones/Crawlers_Nest/mobs/Demonic_Tiphia.lua
+++ scripts/zones/Crawlers_Nest/mobs/Demonic_Tiphia.lua
@@ -4,6 +4,21 @@
 -----------------------------------
 require("scripts/globals/hunts")
 
+function onMobSpawn(mob)
+    mob:setMod(tpz.mod.RESBUILD_SILENCE, 10)
+    mob:setMod(tpz.mod.RESBUILD_LULLABY, 10)
+end
+
+function onMobFight(mob)
+    local delay = mob:getLocalVar("delay")
+    if delay > 25 and mob:actionQueueEmpty() == true and mob:getHPP() <= 50 then -- Uses Cure V repeatedly on cooldown below 50% health
+        mob:castSpell(5) -- Cure V
+        mob:setLocalVar("delay", 0)
+    else
+        mob:setLocalVar("delay", delay+1)
+    end
+end
+
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 236)
 end
