ce622bb125a00ab66563a1d30834b39c5a06e640
Critical <lyardeen@gmail.com>
2021-12-02 10:43:47 -0700

    Amanita will TP faster and HP corrected

--- scripts/zones/West_Ronfaure/mobs/Amanita.lua
+++ scripts/zones/West_Ronfaure/mobs/Amanita.lua
@@ -5,6 +5,14 @@
 require("scripts/globals/hunts")
 ------------------------------
 
+function onMobSpawn(mob)
+    mob:setMod(tpz.mod.REGAIN, 20)
+end
+
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 149)
+end
+
+function onMobDespawn(mob)
+    mob:setRespawnTime(math.random(3600, 4200)) -- 60-70 minutes
 end
