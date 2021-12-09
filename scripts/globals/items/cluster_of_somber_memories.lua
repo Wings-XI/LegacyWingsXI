0a8c1018a6b6792d25b6e0cc4378d50b2e12e96c
Zelita <siknoz@yahoo.com>
2021-12-09 00:10:51 -0800

    Various Item Fixes

---
+++ scripts/globals/items/cluster_of_somber_memories.lua
@@ -0,0 +1,19 @@
+-----------------------------------------
+-- ID: 5291
+-- Cluster of Somber Memories
+-- Turn into a stack of somber memories
+-----------------------------------------
+require("scripts/globals/msg")
+-----------------------------------------
+
+function onItemCheck(target)
+    local result = 0
+    if target:getFreeSlotsCount() == 0 then
+        result = tpz.msg.basic.ITEM_NO_USE_INVENTORY
+    end
+    return result
+end
+
+function onItemUse(target)
+    target:addItem(1611, 12)
+end
---
+++ scripts/globals/items/cluster_of_startling_memories.lua
@@ -0,0 +1,19 @@
+-----------------------------------------
+-- ID: 5290
+-- Cluster of Startling Memories
+-- Turn into a stack of startling memories
+-----------------------------------------
+require("scripts/globals/msg")
+-----------------------------------------
+
+function onItemCheck(target)
+    local result = 0
+    if target:getFreeSlotsCount() == 0 then
+        result = tpz.msg.basic.ITEM_NO_USE_INVENTORY
+    end
+    return result
+end
+
+function onItemUse(target)
+    target:addItem(1610, 12)
+end
