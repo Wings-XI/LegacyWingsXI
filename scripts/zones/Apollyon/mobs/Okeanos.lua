eb1273b387bcb132d174c9537cd4b194dd2e78d8
DESKTOP-J453B6L\EZSCH <ezschneider1@gmail.com>
2021-12-09 15:02:42 -0500

    Giant Resists

--- scripts/zones/Apollyon/mobs/Okeanos.lua
+++ scripts/zones/Apollyon/mobs/Okeanos.lua
@@ -6,7 +6,7 @@ require("scripts/globals/limbus")
 local ID = require("scripts/zones/Apollyon/IDs")
 
 function onMobSpawn(mob)
-    mob:setMod(tpz.mod.UDMGRANGE, -100)
+    mob:addMod(tpz.mod.UDMGRANGE, -100)
     mob:setMobMod(tpz.mobMod.ALLI_HATE, 30)
 end
 
