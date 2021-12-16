7f0acf0946acbc751c96155c38612b8b66fcb6a2
Zelita <siknoz@yahoo.com>
2021-12-16 14:54:26 -0800

    ZoneID shifts, Misc Fixes, and Magic Interrupt Fix

--- scripts/zones/Stellar_Fulcrum/IDs.lua
+++ scripts/zones/Stellar_Fulcrum/IDs.lua
@@ -11,17 +11,17 @@ zones[tpz.zone.STELLAR_FULCRUM] =
     text =
     {
         ITEM_CANNOT_BE_OBTAINED       = 6383, -- You cannot obtain the <item>. Come back after sorting your inventory.
-        ITEM_OBTAINED                 = 6390, -- Obtained: <item>.
-        GIL_OBTAINED                  = 6391, -- Obtained <number> gil.
-        KEYITEM_OBTAINED              = 6392, -- Obtained key item: <keyitem>.
+        ITEM_OBTAINED                 = 6388, -- Obtained: <item>.
+        GIL_OBTAINED                  = 6389, -- Obtained <number> gil.
+        KEYITEM_OBTAINED              = 6391, -- Obtained key item: <keyitem>.
         CARRIED_OVER_POINTS           = 7000, -- You have carried over <number> login point[/s].
         LOGIN_CAMPAIGN_UNDERWAY       = 7001, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!<space>
         LOGIN_NUMBER                  = 7002, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
         CANNOT_ENTER_LEVEL_RESTRICTED = 7021, -- Your party is unable to participate because certain members' levels are restricted.
-        CONQUEST_BASE                 = 7050, -- Tallying conquest results...
-        ORB_MESSAGE_OFFSET            = 7524, -- There is a crack in the <item>. It no longer contains a monster.
-        GEMSTONE_SHIMMERS             = 7910, -- Before you, a small gemstone shimmers with light...
-        GEMSTONE_FUSE                 = 7911, -- With a blinding flash of light, the stones in your hand fuse together to form a brilliant crystal
+        CONQUEST_BASE                 = 7049, -- Tallying conquest results...
+        ORB_MESSAGE_OFFSET            = 7523, -- There is a crack in the <item>. It no longer contains a monster.
+        GEMSTONE_SHIMMERS             = 7908, -- Before you, a small gemstone shimmers with light...
+        GEMSTONE_FUSE                 = 7909, -- With a blinding flash of light, the stones in your hand fuse together to form a brilliant crystal
     },
     mob =
     {
