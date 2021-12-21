99ffff2aa7bc16b5955e4ade250e42944f3d15ac
Critical <lyardeen@gmail.com>
2021-12-21 14:45:34 -0700

    Nunyenunc lottery window, immunity and level range corrected

--- scripts/zones/West_Sarutabaruta/mobs/Nunyenunc.lua
+++ scripts/zones/West_Sarutabaruta/mobs/Nunyenunc.lua
@@ -4,6 +4,10 @@
 -----------------------------------
 require("scripts/globals/hunts")
 
+function onMobSpawn(mob)
+    mob:setMod(tpz.mod.LIGHTRES, 50)
+end
+
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 251)
 end
