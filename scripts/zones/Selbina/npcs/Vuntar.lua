b629868b602549057602f8c6ebdfaa371bf16e44
Gweivyth Quartet <admin@mageringnetwork.com>
2021-12-22 22:46:56 +0000

    Update scripts/zones/Selbina/npcs/Vuntar.lua

--- scripts/zones/Selbina/npcs/Vuntar.lua
+++ scripts/zones/Selbina/npcs/Vuntar.lua
@@ -11,7 +11,8 @@ require("scripts/globals/quests")
 
 function onTrade(player, npc, trade)
     if player:getQuestStatus(OTHER_AREAS_LOG, tpz.quest.id.otherAreas.CARGO) ~= QUEST_AVAILABLE then
-        if tonumber(os.date("%j")) ~= player:getCharVar("VuntarCanBuyItem_date") then
+        -- if tonumber(os.date("%j")) ~= player:getCharVar("VuntarCanBuyItem_date") then
+        -- Above line is commented due to being an RMT change.  If you want to use RMT changes on your server, uncomment above line.
             if npcUtil.tradeHas(trade, 4529) then
                 player:startEvent(52, 1) -- Can Buy rolanberry (881 ce)
             elseif npcUtil.tradeHas(trade, 4530) then
