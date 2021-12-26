d3289188aacf4c3fad7d5dd83a4c5d2311059149
Twilight <23407689+NiQ1@users.noreply.github.com>
2021-12-26 22:05:26 +0200

    Implement even item depository NPCs.

--- scripts/zones/Port_Bastok/npcs/Gallagher.lua
+++ scripts/zones/Port_Bastok/npcs/Gallagher.lua
@@ -1,18 +1,23 @@
 -----------------------------------
 -- Area: Port Bastok
 --  NPC: Gallagher
--- Standard Info NPC
+-- Item Depository
 -----------------------------------
 
+require('scripts/globals/item_depository')
+
 function onTrade(player, npc, trade)
+    itemDepoOnTrade(player, npc, trade)
 end
 
 function onTrigger(player, npc)
-    player:startEvent(349)
+    itemDepoOnTrigger(player, npc)
 end
 
 function onEventUpdate(player, csid, option)
+    itemDepoOnEventUpdate(player, csid, option)
 end
 
 function onEventFinish(player, csid, option)
+    itemDepoOnEventFinish(player, csid, option)
 end
--- scripts/zones/Southern_San_dOria/npcs/Poudoruchant.lua
+++ scripts/zones/Southern_San_dOria/npcs/Poudoruchant.lua
@@ -1,18 +1,23 @@
 -----------------------------------
 -- Area: Southern San d'Oria
 --  NPC: Poudoruchant
---  General Info NPC
+--  Item Depository
 -------------------------------------
 
+require('scripts/globals/item_depository')
+
 function onTrade(player, npc, trade)
+    itemDepoOnTrade(player, npc, trade)
 end
 
 function onTrigger(player, npc)
-    player:startEvent(779)
+    itemDepoOnTrigger(player, npc)
 end
 
 function onEventUpdate(player, csid, option)
+    itemDepoOnEventUpdate(player, csid, option)
 end
 
 function onEventFinish(player, csid, option)
+    itemDepoOnEventFinish(player, csid, option)
 end
