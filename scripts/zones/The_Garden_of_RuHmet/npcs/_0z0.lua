733540a02e0b14fa4b62bd4410663c1b7c07c3ba
OPHELIA-PC\helia <heliashelium@gmail.com>
2022-01-01 05:12:23 -0600

    Allow Players To Fight Promathia Without Race Protection

--- scripts/zones/The_Garden_of_RuHmet/npcs/_0z0.lua
+++ scripts/zones/The_Garden_of_RuHmet/npcs/_0z0.lua
@@ -20,7 +20,7 @@ end
 function onTrigger(player, npc)
     --player:addMission(COP, tpz.mission.id.cop.WHEN_ANGELS_FALL)
     --player:setCharVar("PromathiaStatus", 3)
-    if (player:getCurrentMission(COP) == tpz.mission.id.cop.WHEN_ANGELS_FALL and player:getCharVar("PromathiaStatus")==3) then
+    if (player:getCurrentMission(COP) == tpz.mission.id.cop.WHEN_ANGELS_FALL and (player:getCharVar("PromathiaStatus")==2 or player:getCharVar("PromathiaStatus")==3)) then
         player:startEvent(203)
     elseif (EventTriggerBCNM(player, npc)) then
     elseif (player:getCurrentMission(COP) == tpz.mission.id.cop.WHEN_ANGELS_FALL and player:getCharVar("PromathiaStatus")==5) then
