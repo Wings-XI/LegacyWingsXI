fbbf18db2a51b48cbca89dd03863ea2f9f7d7558
OPHELIA-PC\helia <heliashelium@gmail.com>
2022-01-10 08:29:19 -0600

    Final cleanup of mistakes

--- scripts/globals/spells/phalanx_ii.lua
+++ scripts/globals/spells/phalanx_ii.lua
@@ -13,16 +13,18 @@ end
 function onSpellCast(caster, target, spell)
     local enhskill = caster:getSkillLevel(tpz.skill.ENHANCING_MAGIC)
     local power = 0
-    local merits = caster:getMerit(tpz.merit.PARALYZE_II)
+    local merits = caster:getMerit(tpz.merit.PHALANX_II)
 
     -- Duration Caps at 240s w/ +30s per merit (https://ffxiclopedia.fandom.com/wiki/Phalanx_II?oldid=1009846)
     local duration = calculateDuration(90, spell:getSkillType(), spell:getSpellGroup(), caster, target)
     -- Implementing merit bonuses prior to level calculation as all testing is presumed done at 75
-    duration = duration + (30 * merits)
+    duration = duration + (merits * 10)
+    print("Duration :".. duration)
     duration = calculateDurationForLvl(duration, 75, target:getMainLvl())
 
     -- Power Formula Sources: (http://nullvector.com/enhancing.php, https://www.bg-wiki.com/index.php?title=Phalanx_II&oldid=65695, and https://ffxiclopedia.fandom.com/wiki/Phalanx_II?oldid=1009846)
-    power = math.max(math.floor((enhskill / 25) + (1 + (3 * merits))), 0)
+    power = math.max(math.floor((enhskill / 25) + (1 + merits)), 0)
+    print("Power: ".. power)
 
     if target:addStatusEffect(tpz.effect.PHALANX, power, 0, duration) then
         spell:setMsg(tpz.msg.basic.MAGIC_GAIN_EFFECT)
