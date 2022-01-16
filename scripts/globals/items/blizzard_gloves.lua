20df98ca8af5290181d925b83ebfc1024f79a6c6
OPHELIA-PC\helia <heliashelium@gmail.com>
2022-01-16 06:45:22 -0600

    Fix Enspell Enhancement Items

--- scripts/globals/items/blizzard_gloves.lua
+++ scripts/globals/items/blizzard_gloves.lua
@@ -10,5 +10,17 @@ end
 
 function onItemUse(target)
     local effect = tpz.effect.ENBLIZZARD
-    doEnspell(target, target, nil, effect)
+    local duration = 180
+    local magicskill = target:getSkillLevel(tpz.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end
