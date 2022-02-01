224cee00372bedda367458cc6528bc43c0880a8d
OPHELIA-PC\helia <heliashelium@gmail.com>
2022-02-01 17:14:05 -0600

    Apoc Nigh Tuning

--- scripts/zones/Empyreal_Paradox/mobs/Ealdnarche.lua
+++ scripts/zones/Empyreal_Paradox/mobs/Ealdnarche.lua
@@ -3,6 +3,7 @@
 --  Mob: Eald'narche
 -- Apocalypse Nigh Final Fight
 -----------------------------------
+require("scripts/globals/utils")
 
 function onMobInitialize(mob)
     mob:setMobMod(tpz.mobMod.TELEPORT_CD, 30)
@@ -16,9 +17,32 @@ function onMobSpawn(mob)
     mob:addMod(tpz.mod.UFASTCAST, 60)
     mob:addMod(tpz.mod.UDMGMAGIC, -95)
     mob:addMod(tpz.mod.UDMGPHYS, -40)
-    mob:addMod(tpz.mod.EVA, 10)
+    mob:addMod(tpz.mod.EVA, 50)
     mob:setMobMod(tpz.mobMod.HP_STANDBACK, -1)
     mob:setMobMod(tpz.mobMod.GA_CHANCE, 25)
+    mob:addStatusEffect(tpz.effect.REFRESH, 100, 1, 0)
+end
+
+function onMobFight(mob, target)
+    if os.time() > mob:getLocalVar("nextTeleport") then
+        mob:setLocalVar("currentTP", mob:getTP())
+        mob:useMobAbility(989) -- Warp Out
+        mob:setLocalVar("nextTeleport", os.time() + 20)
+    end
+
+    if utils.getTargetDistance(mob, target) >= 3 then
+        if os.time() > mob:getLocalVar("nextSpellCast") then
+            mob:castSpell()
+            mob:setLocalVar("nextSpellCast", os.time() + 10)
+        end
+    end
+end
+
+function onMobWeaponSkill(target, mob, skill)
+    local skillId = skill:getID()
+    if skillId == 988 or 989 then -- Warp In or Out Restore TP
+        mob:setTP(mob:getLocalVar("currentTP"))
+    end
 end
 
 function onMobDeath(mob, player, isKiller)
