134d43721b5fc1c5c9033d271976192c9c3ef6b0
Lyrics <10466116-LyricsToSongs@users.noreply.gitlab.com>
2022-03-10 21:23:45 +0000

    Adds many missing dialogues with NPCs in Bastok Markets [S]

---
+++ scripts/zones/Bastok_Markets_[S]/npcs/Dunbaff.lua
@@ -0,0 +1,12 @@
+----------------------------------
+-- Area: Bastok Markets [S]
+--  NPC: Dunbaff
+-- Type: Quest NPC (Light in the Darkness, Storm on the Horizon)
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end
+
+function onTrigger(player,npc)
+    player:startEvent(156)
+end
