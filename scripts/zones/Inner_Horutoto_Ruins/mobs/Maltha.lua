d199a7117ff3af3f5182182f51bc335a169ecf08
Critical <lyardeen@gmail.com>
2022-03-23 13:17:34 -0600

    Maltha immunity and behavior corrected

--- scripts/zones/Inner_Horutoto_Ruins/mobs/Maltha.lua
+++ scripts/zones/Inner_Horutoto_Ruins/mobs/Maltha.lua
@@ -4,6 +4,11 @@
 -----------------------------------
 require("scripts/globals/hunts")
 
+function onMobSpawn(mob)
+    -- Has a very strong innate Haste effect, similar to Hundred Fists.
+    mob:setMod(tpz.mod.DELAY, 500)
+end
+
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 288)
 end
