2b5d794bd254fbc22faad4ea7c56bb02fd2bed01
Zelita <7776472-siknoz@users.noreply.gitlab.com>
2022-04-09 19:05:56 +0000

    Fix Mushroom Stealing Crashes

--- scripts/zones/North_Gustaberg_[S]/mobs/Coppercap.lua
+++ scripts/zones/North_Gustaberg_[S]/mobs/Coppercap.lua
@@ -10,14 +10,17 @@ require("scripts/globals/world")
 -----------------------------------
 
 function onMobSpawn(mob)
+    mob:setStealItemID(4373) -- Woozyshroom
 end
 
 function onMobEngaged(mob, target)
-    mob:addListener("ITEM_STOLEN", "SHROOM_ITEM_STOLEN", function(mob, player, itemId)
+    mob:addListener("ITEM_STOLEN", "ITEM_STOLEN", function(mob, player, itemId)
         if itemId == 4373 then
             mob:AnimationSub(1)
-        elseif itemId == 4375 then 
+            mob:setStealItemID(4375) -- Danceshroom
+        elseif itemId == 4375 then
             mob:AnimationSub(2)
+            mob:setStealItemID(5680) -- Agaricus
         elseif itemId == 5680 then
             mob:AnimationSub(3)
         end
