17ca4b03ae7bd0c67c96b47239d16859e56016f0
Laura Yardeen <lyardeen@gmail.com>
2022-04-15 17:29:26 -0600

    Deal with key items entering BCNMs better

--- scripts/zones/Sacrificial_Chamber/bcnms/project_shantottofication.lua
+++ scripts/zones/Sacrificial_Chamber/bcnms/project_shantottofication.lua
@@ -20,75 +20,59 @@ function onBattlefieldEnter(player, battlefield)
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_GREED) then
         player:addStatusEffectEx(tpz.effect.BARTHUNDER, tpz.effect.MAGIC_DEF_BOOST, 0, 0, 0)
         player:setMod(tpz.mod.THUNDERRES, 150)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_GREED)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_ENVY) then
         player:addStatusEffectEx(tpz.effect.BARBLIZZARD, tpz.effect.MAGIC_DEF_BOOST, 0, 0, 0)
         player:setMod(tpz.mod.ICERES, 150)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_ENVY)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_MALICE) then
         player:addStatusEffectEx(tpz.effect.BARAERO, tpz.effect.MAGIC_DEF_BOOST, 0, 0, 0)
         player:setMod(tpz.mod.WINDRES, 150)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_MALICE)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_DECEIT) then
         player:addStatusEffectEx(tpz.effect.BARSTONE, tpz.effect.MAGIC_DEF_BOOST, 0, 0, 0)
         player:setMod(tpz.mod.EARTHRES, 150)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_DECEIT)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_PRIDE) then
         player:addStatusEffectEx(tpz.effect.BARFIRE, tpz.effect.MAGIC_DEF_BOOST, 0, 0, 0)
         player:setMod(tpz.mod.FIRERES, 150)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_PRIDE)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_BALE) then
         player:addStatusEffectEx(tpz.effect.BARWATER, tpz.effect.MAGIC_DEF_BOOST, 0, 0, 0)
         player:setMod(tpz.mod.WATERRES, 150)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_BALE)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_DESPAIR) then
         player:addStatusEffect(tpz.effect.STR_BOOST, 150, 0, 0)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_DESPAIR)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_REGRET) then
         player:addStatusEffect(tpz.effect.DEX_BOOST, 150, 0, 0)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_REGRET)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_RAGE) then
         player:addStatusEffect(tpz.effect.VIT_BOOST, 150, 0, 0)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_RAGE)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_AGONY) then
         player:addStatusEffect(tpz.effect.AGI_BOOST, 150, 0, 0)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_AGONY)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_DOLOR) then
         player:addStatusEffect(tpz.effect.INT_BOOST, 150, 0, 0)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_DOLOR)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_RANCOR) then
         player:addStatusEffect(tpz.effect.MND_BOOST, 150, 0, 0)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_RANCOR)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_STRIFE) then
         player:addStatusEffect(tpz.effect.CHR_BOOST, 150, 0, 0)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_STRIFE)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_PENURY) then
         local hpBoost = player:getMaxHP() * 3
         player:addStatusEffect(tpz.effect.MAX_HP_BOOST, hpBoost, 0, 0)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_PENURY)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_BLIGHT) then
         local mpBoost = player:getMaxMP() * 3
         player:addStatusEffect(tpz.effect.MAX_MP_BOOST, mpBoost, 0, 0)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_BLIGHT)
     end
     if player:hasKeyItem(tpz.ki.TABLET_OF_HEXES_DEATH) then
         player:addStatusEffect(tpz.effect.RERAISE, 1, 0, 3600)
         player:setLocalVar("Shantottofication", 1)
-        player:delKeyItem(tpz.ki.TABLET_OF_HEXES_DEATH)
     end
 
     player:getStatusEffect(tpz.effect.BARTHUNDER):unsetFlag(tpz.effectFlag.DEATH)
@@ -153,6 +137,23 @@ function onBattlefieldLeave(player, battlefield, leavecode)
     player:delStatusEffect(tpz.effect.MAX_HP_BOOST)
     player:delStatusEffect(tpz.effect.MAX_MP_BOOST)
     player:setLocalVar("Shantottofication", 0)
+
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_GREED)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_ENVY)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_MALICE)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_DECEIT)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_PRIDE)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_BALE)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_DESPAIR)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_REGRET)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_RAGE)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_AGONY)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_DOLOR)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_RANCOR)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_STRIFE)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_PENURY)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_BLIGHT)
+    player:delKeyItem(tpz.ki.TABLET_OF_HEXES_DEATH)
 end
 
 function onEventUpdate(player, csid, option)
