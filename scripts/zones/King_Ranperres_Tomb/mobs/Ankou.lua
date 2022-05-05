943522db02dc4665c0d99169f51922822a08e422
Ari <gregm@osmstudios.com>
2022-05-04 20:44:52 -0600

    Fix Ankou and Nachzehrer spawntype

--- scripts/zones/King_Ranperres_Tomb/mobs/Ankou.lua
+++ scripts/zones/King_Ranperres_Tomb/mobs/Ankou.lua
@@ -7,23 +7,11 @@ local ID = require("scripts/zones/King_Ranperres_Tomb/IDs")
 ------------------------------
 
 function onMobRoam(mob)
-    local hour = VanadielHour()
-
-    if hour >= 6 and hour < 18 then -- Despawn Shii if its day
-        DespawnMob(mob:getID())
-    end
 end
 
 function onMobDisengage(mob)
-    local hour = VanadielHour()
-
-    if hour >= 6 and hour < 18 then -- Despawn Shii if its day
-        DespawnMob(mob:getID())
-    end
 end
 
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 176)
-    mob:setLocalVar("Respawn", os.time() + 3600)
-    DisallowRespawn(ID.mob.ANKOU, true)
 end
