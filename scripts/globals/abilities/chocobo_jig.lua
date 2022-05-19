c88ead35ec4a328f3a156e4b021aabbac60fc00c
Caelic <bclynn96@gmail.com>
2022-05-19 16:55:21 -0400

    flee and jig now remove gravity per era

--- scripts/globals/abilities/chocobo_jig.lua
+++ scripts/globals/abilities/chocobo_jig.lua
@@ -16,6 +16,7 @@ function onAbilityCheck(player, target, ability)
 end
 
 function onUseAbility(player, target, ability)
+    if player:hasStatusEffect(tpz.effect.WEIGHT) then player:delStatusEffect(tpz.effect.WEIGHT) end
     local baseDuration = 120
     local durationMultiplier = 1.0 + utils.clamp(player:getMod(tpz.mod.CHOCOBO_JIG_DURATION), 0, 50) / 100
     local finalDuration = math.floor(baseDuration * durationMultiplier)
