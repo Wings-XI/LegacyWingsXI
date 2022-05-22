727c36caa05aa8419e17724f3621fbeacc1cf48b
Caelic <bclynn96@gmail.com>
2022-05-22 06:54:19 +0000

    fix absorb-str resist

--- scripts/globals/spells/absorb-str.lua
+++ scripts/globals/spells/absorb-str.lua
@@ -29,7 +29,6 @@ function onSpellCast(caster, target, spell)
             spell:setMsg(tpz.msg.basic.MAGIC_RESIST)
         else
             spell:setMsg(tpz.msg.basic.MAGIC_ABSORB_STR)
-            resist = 0.25
             caster:addStatusEffect(tpz.effect.STR_BOOST, ABSORB_SPELL_AMOUNT*resist*((100+(caster:getMod(tpz.mod.AUGMENTS_ABSORB)))/100), ABSORB_SPELL_TICK, ABSORB_SPELL_AMOUNT*ABSORB_SPELL_TICK*resist) -- caster gains STR
             target:addStatusEffect(tpz.effect.STR_DOWN, ABSORB_SPELL_AMOUNT*resist*((100+(caster:getMod(tpz.mod.AUGMENTS_ABSORB)))/100), ABSORB_SPELL_TICK, ABSORB_SPELL_AMOUNT*ABSORB_SPELL_TICK*resist)    -- target loses STR
         end
