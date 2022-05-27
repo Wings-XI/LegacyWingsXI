536923ea34b20d01b0dd7a42df9ee8d554c18888
Ramzah <ramzah@rocketmail.com>
2022-05-27 21:43:20 +0200

    Fixed and formatted smn merit BPs

--- scripts/globals/abilities/pets/wind_blade.lua
+++ scripts/globals/abilities/pets/wind_blade.lua
@@ -13,17 +13,16 @@ function onAbilityCheck(player, target, ability)
 end
 
 function onPetAbility(target, pet, skill)
-
+    local ele = tpz.damageType.WIND
+    local coe = getAvatarEcosystemCoefficient(target, ele)
     local dINT = math.floor(pet:getStat(tpz.mod.INT) - target:getStat(tpz.mod.INT))
     local tp = skill:getTP() / 10
     local master = pet:getMaster()
     local merits = 0
     if (master ~= nil and master:isPC()) then
-        merits = master:getMeritCount(tpz.merit.WIND_BLADE)*40
+        merits = master:getMerit(tpz.merit.WIND_BLADE)*40
     end
     
-    local ele = tpz.damageType.WIND
-    local coe = getAvatarEcosystemCoefficient(target, ele)
 
     tp = tp + (merits - 40)
     if (tp > 300) then
@@ -31,8 +30,8 @@ function onPetAbility(target, pet, skill)
     end
 
     --note: this formula is only accurate for level 75 - 76+ may have a different intercept and/or slope
-    local damage = math.floor((512 + 1.66*(tp+1))*coe)
-    damage = damage + (dINT * 1.5)
+    local damage = math.floor((512 + 1.72*(tp+1))*coe)
+    damage = math.floor(damage + (dINT * 1.5))
     damage = MobMagicalMove(pet, target, skill, damage, tpz.magic.ele.WIND, 1, TP_NO_EFFECT, 0)
     damage = mobAddBonuses(pet, nil, target, damage.dmg, tpz.magic.ele.WIND)
     damage = AvatarFinalAdjustments(damage, pet, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WIND, 1)
