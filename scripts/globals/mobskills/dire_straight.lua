cef0e8f8d7d9f60b72bff05ee2a529605adb689e
Chief <7995489-Chief_@users.noreply.gitlab.com>
2022-06-09 14:05:26 +0300

    add Dire Straight mob skill

---
+++ scripts/globals/mobskills/dire_straight.lua
@@ -0,0 +1,27 @@
+---------------------------------------------
+--  Dire Straight
+--
+--  Description: Deals damage to a single target.
+--  Type: Physical
+--  Wipes shadows
+---------------------------------------------
+require("scripts/globals/settings")
+require("scripts/globals/status")
+require("scripts/globals/monstertpmoves")
+
+function onMobSkillCheck(target, mob, skill)
+    return 0
+end
+
+function onMobWeaponSkill(target, mob, skill)
+
+    local numhits = 1
+    local accmod = 2
+    local dmgmod = 2
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, MOBPARAM_IGNORE_SHADOWS)
+
+    target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
+	if dmg > 0 and skill:getMsg() ~= 31 then target:tryInterruptSpell(mob, info.hitslanded) end
+    return dmg
+end
