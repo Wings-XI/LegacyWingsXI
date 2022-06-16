21955125c851f5515f8d48fe96b54570bb7444a9
Chief <7995489-Chief_@users.noreply.gitlab.com>
2022-06-16 12:42:13 +0300

    add Rising Swell script

---
+++ scripts/globals/mobskills/rising_swell.lua
@@ -0,0 +1,21 @@
+---------------------------------------------
+--  Rising Swell
+--  Description: Removes all Equipment
+--  Type: Magical Enfeebling
+--  Ignore Shadows, Area of Effect
+---------------------------------------------
+require("scripts/globals/status")
+require("scripts/globals/monstertpmoves")
+---------------------------------------------
+function onMobSkillCheck(target, mob, skill)
+    return 0
+end
+
+function onMobWeaponSkill(target, mob, skill)
+    for i = tpz.slot.MAIN, tpz.slot.BACK do
+        target:unequipItem(i)
+    end
+    skill:setMsg(tpz.msg.basic.NONE)
+    return 0
+end
+
