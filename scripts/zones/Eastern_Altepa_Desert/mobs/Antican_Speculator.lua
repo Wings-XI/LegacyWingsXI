255c76f9ab4b1a7881907842b97a4495883a16a0
Chiefy <zrhauseman@gmail.com>
2022-07-05 20:24:24 -0500

    Onpath(Mob) Additions

--- scripts/zones/Eastern_Altepa_Desert/mobs/Antican_Speculator.lua
+++ scripts/zones/Eastern_Altepa_Desert/mobs/Antican_Speculator.lua
@@ -2,8 +2,26 @@
 -- Area: Eastern Altepa Desert
 --  Mob: Antican Speculator
 -----------------------------------
+local ID = require("scripts/zones/Eastern_Altepa_Desert/IDs")
 require("scripts/globals/regimes")
+require("scripts/globals/status")
+require("scripts/globals/mobs")
+require("scripts/globals/pathfind")
 -----------------------------------
+
+function onMobSpawn(mob)
+    onPath(mob)
+end
+
+function onPath(mob)
+        local centurio = GetMobByID(ID.mob.CENTURIO_XII_I)
+        if centurio:isSpawned() then
+            local mobId = mob:getID()
+            switch (mobId): caseof {
+                [17244374] = function() mob:pathTo(centurio:getXPos() - 3, centurio:getYPos(), centurio:getZPos() - 3) end,
+            }
+        end
+end
 
 function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 112, 3, tpz.regime.type.FIELDS)
