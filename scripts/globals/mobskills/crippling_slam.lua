c2c4d40a6743c16affeb8dd759ae65779eb104da
Chiefy <zrhauseman@gmail.com>
2022-08-06 01:13:01 -0500

    Ready for first test

--- scripts/globals/mobskills/crippling_slam.lua
+++ scripts/globals/mobskills/crippling_slam.lua
@@ -1,7 +1,7 @@
 ---------------------------------------------
---  Tourbillion
+--  Crippling Slam
 --
---  Description: Delivers an area attack. Additional effect duration varies with TP. Additional effect: Weakens defense.
+--  Description: Delivers an area attack. Additional effect duration varies with TP. Additional effect: Paralysis.
 --  Type: Physical
 --  Shadow per hit
 --  Range: Unknown range
@@ -21,9 +21,9 @@ function onMobWeaponSkill(target, mob, skill)
     local dmgmod = 1.5
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded)
-    local duration = 20 * (skill:getTP() / 1000)
+    local duration = 20 * (math.random(1,2) / 1)
 
-    MobPhysicalStatusEffectMove(mob, target, skill, tpz.effect.PARALYSIS, 20, 0, duration)
+    MobPhysicalStatusEffectMove(mob, target, skill, tpz.effect.PARALYSIS, 30, 0, duration)
 
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT)
 	if dmg > 0 and skill:getMsg() ~= 31 then target:tryInterruptSpell(mob, info.hitslanded) end
--- scripts/zones/Bhaflau_Thickets/mobs/Dea.lua
+++ scripts/zones/Bhaflau_Thickets/mobs/Dea.lua
@@ -1,43 +1,49 @@
 -----------------------------------
 -- Area: Bhaflau Thickets
 --  ZNM: Dea(T3ZNM)
--- To do: As HP decreases, the effective range of "Demoralizing Roar" and "Cripling Slam" expands. -- Need more evidence.
+-- To do:   As HP decreases, the effective range of "Demoralizing Roar" and "Crippling Slam" expands. -- Need more evidence.
+--          Dispel message for boiling blood needs to fixed.
+-- Issues:  If mob is never moved after spawning the target:isBehind/isInFront can react weirdly.
 -----------------------------------
 --mixins = {require("scripts/mixins/rage")}
 require("scripts/globals/status")
 -----------------------------------
 
+function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.IDLE_DESPAWN, 300)
+end
+
 function onMobSpawn(mob)
-    mob:setLocalVar("SetAbility", math.random(1,3))
     mob:setLocalVar("QuedAbility", 0)
-    mob:setLocalVar("Crippling", 0)
 end
 
 function onMobWeaponSkillPrepare(mob, target)
-    local SetAbility = mob:getLocalVar("SetAbility")
-    local QuedAbility = mob:getLocalVar("QuedAbility")
-    local Crippling = mob:getLocalVar("Crippling")
-
-    if skill:getID() == 2101 then
-        Crippling = 1
-    end
-    if skill:getID() == 2102 then
-        if SetAbility = 1 then
-            QuedAbility = 1
-        elseif SetAbility = 2 or 3 then
-            QuedAbility = 2
-        end
-        --mob:setLocalVar("SetAbility", math.random(1,3))
-        --mob:useMobAbility(QuedAbility)
-    end
-    return 2101
+    return 0
 end
 
 function onMobWeaponSkill(target, mob, skill)
+    local QuedAbility = mob:getLocalVar("QuedAbility")
+
+    if skill:getID() == 2101 then
+        mob:useMobAbility(2104)
+    end
+    if skill:getID() == 2102 then
+        mob:setLocalVar("QuedAbility", math.random(1,3))
+        if QuedAbility == 1 then
+            mob:useMobAbility(2104)
+        elseif QuedAbility == 2 or 3 then
+            if target:isBehind(mob, 96) then
+                mob:useMobAbility(2099)
+            elseif target:isInfront(mob, 90) then
+                mob:useMobAbility(2100)
+            else
+                mob:useMobAbility(2104)
+            end
+        end
+    end
     if skill:getID() == 2099 or 2100 or 2104 then
-        Crippling = 0
-        QuedAbility = 0
-        SetAbility = math.random(1,3)
+        mob:setLocalVar("QuedAbility", 0)
+    end   
 end
 
 function onMobFight(mob, target)
@@ -45,19 +51,6 @@ function onMobFight(mob, target)
         mob:setMod(tpz.mod.DOUBLE_ATTACK, 100)
     else
         mob:setMod(tpz.mod.DOUBLE_ATTACK, 10)
-    end
-
-    if Crippling = 1 then
-        mob:useMobAbility(2104)
-    end
-    if QuedAbility = 1 then
-        mob:useMobAbility(2104)
-    elseif QuedAbility = 2 then
-        if target:isBehind(mob, 96) then
-            mob:useMobAbility(2100)
-        elseif target:isInfront(mob, 90) then
-            mob:useMobAbility(2099)
-        end
     end
 end
 
