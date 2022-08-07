6870402926acc5d91429a6b3b812052d8dfaa9bc
MowFord <lbegnaud@radersolutions.com>
2022-08-07 16:21:20 +0000

    Brasscap steal fix

--- scripts/zones/North_Gustaberg_[S]/mobs/Coppercap.lua
+++ scripts/zones/North_Gustaberg_[S]/mobs/Coppercap.lua
@@ -10,25 +10,54 @@ require("scripts/globals/world")
 -----------------------------------
 
 function onMobSpawn(mob)
-    mob:setStealItemID(4373) -- Woozyshroom
-end
-
-function onMobEngaged(mob, target)
-    mob:addListener("ITEM_STOLEN", "ITEM_STOLEN", function(mob, player, itemId)
-        if itemId == 4373 then
+    updateRegen(mob)
+    mob:AnimationSub(0)
+    mob:setStealItemID(4373) -- Woozyshroom is default steal item, needed here in case first action on the mob is steal
+    mob:addListener("ITEM_STOLEN", "SHROOM_ITEM_STOLEN", function(mob, player, itemId)
+        if mob:AnimationSub() == 0 then
             mob:AnimationSub(1)
-            mob:setStealItemID(4375) -- Danceshroom
-        elseif itemId == 4375 then
+        elseif mob:AnimationSub() == 1 then
             mob:AnimationSub(2)
-            mob:setStealItemID(5680) -- Agaricus
-        elseif itemId == 5680 then
+        elseif mob:AnimationSub() == 2 then
             mob:AnimationSub(3)
         end
     end)
 end
 
+function onMobInitialize(mob)
+end
+
+function onMobEngaged(mob, target)
+end
+
+function onMobFight(mob)
+    updateRegen(mob)
+    
+    -- adjust steal item based on the animationsub. This is adjusted by either successfully stealing or by the mob using numbshroom or queezyshroom
+    if mob:AnimationSub() == 0 then
+        mob:setStealItemID(4373) -- Woozyshroom
+    elseif mob:AnimationSub() == 1 then
+        mob:setStealItemID(4375) -- Danceshroom
+    elseif mob:AnimationSub() == 2 then
+        mob:setStealItemID(5680) -- Agaricus
+    elseif mob:AnimationSub() == 3 then
+        mob:itemStolen() -- Nothing left to steal
+    end
+end
+
+function onMobRoam(mob)
+    updateRegen(mob)
+end
+
+function updateRegen(mob)
+    if mob:getWeather() == tpz.weather.RAIN or mob:getWeather() == tpz.weather.SQUALL then
+        mob:setMod(tpz.mod.REGEN, 9) -- 1% per 12s from retail capture
+    else
+        mob:setMod(tpz.mod.REGEN, 0)
+    end
+end
+
 function onMobDisengage(mob)
-    mob:removeListener("ITEM_STOLEN")
 end
 
 function onMobDeath(mob, player, isKiller)
@@ -36,4 +65,5 @@ end
 
 function onMobDespawn(mob)
     tpz.mob.phOnDespawn(mob, ID.mob.GLOOMANITA_PH, 10, 3600) -- 1 hour
+    mob:removeListener("SHROOM_ITEM_STOLEN")
 end
