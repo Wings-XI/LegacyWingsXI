ab9947947cb895fc19f15fea5595e0000317bc55
lbegnaud <lbegnaud@radersolutions.com>
2022-08-28 13:57:21 -0500

    fix boost to re-apply attp on zone/relog

--- scripts/globals/effects/boost.lua
+++ scripts/globals/effects/boost.lua
@@ -7,6 +7,7 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
+    target:addMod(tpz.mod.ATTP, effect:getPower() / 100)
 end
 
 function onEffectTick(target, effect)
