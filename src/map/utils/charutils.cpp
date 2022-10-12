94fd59f274fa981b10c78daeb937e29ecb8a75eb
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-10-11 22:53:21 -0400

    Gilfinder applying to all party member equally

--- src/map/utils/charutils.cpp
+++ src/map/utils/charutils.cpp
@@ -3862,14 +3862,37 @@ namespace charutils
             // all members might not be in range
             if (members.size() > 0)
             {
+<<<<<<< HEAD
                 // distribute gil
                 int32 gilPerPerson = static_cast<int32>(gil / members.size());
+=======
+                // Distribute Gil
+                int32 gilPerPerson    = static_cast<int32>(gil / members.size());
+                int16 gilFinderActive = 0;
+
+>>>>>>> de0cb63cad (Gilfinder applying to all party member equally)
                 for (auto PMember : members)
                 {
-                    // Check for gilfinder
-                    gilPerPerson += gilPerPerson * PMember->getMod(Mod::GILFINDER) / 100;
-                    UpdateItem(PMember, LOC_INVENTORY, 0, gilPerPerson);
-                    PMember->pushPacket(new CMessageBasicPacket(PMember, PMember, gilPerPerson, 0, 565));
+                    // Check for highest gilfinder tier
+                    if (PMember->getMod(Mod::GILFINDER) > gilFinderActive)
+                    {
+                        gilFinderActive = PMember->getMod(Mod::GILFINDER);
+                    }
+                }
+                for (auto PMember : members)
+                {
+                    // Distributte gilfinder gil
+                    if (gilFinderActive > 0)
+                    {
+                        int32 memberGil = gilPerPerson * (100 + gilFinderActive) / 100;
+                        UpdateItem(PMember, LOC_INVENTORY, 0, memberGil);
+                        PMember->pushPacket(new CMessageBasicPacket(PMember, PMember, memberGil, 0, 565));
+                    }
+                    else
+                    {
+                        UpdateItem(PMember, LOC_INVENTORY, 0, gilPerPerson);
+                        PMember->pushPacket(new CMessageBasicPacket(PMember, PMember, gilPerPerson, 0, 565));
+                    }
                 }
             }
         }
