0b8f3b7e245b286f1217d1a29c6255ba973a1210
lbegnaud <lbegnaud@radersolutions.com>
2022-11-23 09:24:49 -0600

    initial changes

--- scripts/zones/Horlais_Peak/mobs/Fighting_Sheep.lua
+++ scripts/zones/Horlais_Peak/mobs/Fighting_Sheep.lua
@@ -7,9 +7,24 @@
 require("scripts/globals/status")
 -----------------------------------
 
+
 function onMobInitialize(mob)
     mob:setMod(tpz.mod.ICERES, 75)
+    mob:setMod(tpz.mod.SLEEPRES, 100)
+    mob:setMod(tpz.mod.LULLABYRES, 100)
+    mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
+    mob:setMobMod(tpz.mobMod.CHARMABLE, 0)
 end
+
+function onAdditionalEffect(mob, target, damage)
+    -- 30 yalm knockback
+    return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.KNOCKBACK, { power = 30, chance = 100 })
+end
+
+function onMobSpawn(mob)
+    mob:setMod(tpz.mod.HASTE_GEAR, 5000) -- 50% haste
+end
+
 
 function onMobDeath(mob, player, isKiller)
 end
