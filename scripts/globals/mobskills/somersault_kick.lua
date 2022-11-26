23395e9b07a7866b5d297438085fda758dc39ea4
Spaceballs <tedbstanton@gmail.com>
2022-11-26 12:18:13 -0700

    forgot one

--- scripts/globals/mobskills/somersault_kick.lua
+++ scripts/globals/mobskills/somersault_kick.lua
@@ -14,16 +14,25 @@ require("scripts/globals/monstertpmoves")
 ---------------------------------------------
 function onMobSkillCheck(target, mob, skill)
 
-  if(mob:getFamily() == 91) then
-    local mobSkin = mob:getModelId()
+    if(mob:getFamily() == 91) then
+        local mobSkin = mob:getModelId()
 
-    if (mobSkin == 1639) then
-        return 0
-    else
-        return 1
+        if (mobSkin == 1639) then
+            return 0
+        else
+            return 1
+        end
     end
-  end
-    return 0
+
+    if mob:getID() == 16986430 then
+        if mob:getLocalVar("WarmUp") == 1 then
+            return 1
+        else
+            return 0
+        end   
+    else
+        return 0
+    end
 end
 
 function onMobWeaponSkill(target, mob, skill)
