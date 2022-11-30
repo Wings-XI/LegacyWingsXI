2fa49fd82d04b3318c3af8c8141c9e543accdfb7
lbegnaud <lbegnaud@radersolutions.com>
2022-11-29 19:37:16 -0600

    tweak troll cuirasser SE logic

--- scripts/zones/Mount_Zhayolm/mobs/Troll_Cuirasser.lua
+++ scripts/zones/Mount_Zhayolm/mobs/Troll_Cuirasser.lua
@@ -17,20 +17,17 @@ end
 
 function onAdditionalEffect(mob, target, damage)
     -- fudges souleater logic instead of messing with core for the one mob in the game that uses souleater
+    -- Note that this doesn't affect mobskill dmg nor does it affect dmg during blood weapon
     if mob:hasStatusEffect(tpz.effect.SOULEATER) then
         local hpToDmg = 0
-        if mob:getHP() > 20 then
-            local hpToDmg = mob:getHP() * 12 / 100
-            hpToDmg = math.max(hpToDmg, 20)
-            mob:setHP(mob:getHP() - hpToDmg)-- Due to resistance checking, best to floor this at 20
-
-            return tpz.mob.onAddEffect(mob, target, hpToDmg, tpz.mob.ae.ENDARK)
-
-        else
-            local hpToDmg = 0
-            return tpz.mob.onAddEffect(mob, target, hpToDmg, tpz.mob.ae.ENDARK)
+        -- avoid the ability to kill the mob from souleater hp reduction
+        if mob:getHPP() > 1 then
+            hpToDmg = math.ceil(mob:getHP() * 12 / 100)
+            mob:setHP(mob:getHP() - hpToDmg)
         end
-
+        hpToDmg = math.max(hpToDmg, 20) -- Due to resistance checking, best to floor this at 20, and double it went sending dmg
+        -- printf("dmg %u", hpToDmg)
+        return tpz.mob.onAddEffect(mob, target, 2 * hpToDmg, tpz.mob.ae.ENDARK)
     end
 end
 
