9244cd2b90707fc053fa107695ae441974931749
Gweivyth Quartet <admin@mageringnetwork.com>
2022-12-05 15:16:35 +0000

    Update sql-wings/skill_ranks.sql

--- sql-wings/skill_ranks.sql
+++ sql-wings/skill_ranks.sql
@@ -70,7 +70,7 @@ INSERT INTO `skill_ranks` VALUES(7, 'scythe', 3, 0, 0, 10, 0, 0, 0, 1, 5, 0, 0, 
 INSERT INTO `skill_ranks` VALUES(8, 'polearm', 5, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0);
 INSERT INTO `skill_ranks` VALUES(9, 'katana', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 INSERT INTO `skill_ranks` VALUES(10, 'great katana', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-INSERT INTO `skill_ranks` VALUES(11, 'club', 5, 6, 3, 6, 9, 10, 2, 8, 9, 9, 10, 10, 10, 10, 6, 5, 0, 9, 0, 6, 3, 8);
+INSERT INTO `skill_ranks` VALUES(11, 'club', 5, 6, 1, 6, 9, 10, 2, 8, 9, 9, 10, 10, 10, 10, 6, 5, 0, 9, 0, 6, 3, 8);
 INSERT INTO `skill_ranks` VALUES(12, 'staff', 4, 4, 6, 5, 0, 0, 2, 0, 0, 6, 0, 0, 0, 5, 4, 0, 0, 0, 0, 6, 6, 0);
 INSERT INTO `skill_ranks` VALUES(25, 'archery', 9, 0, 0, 0, 9, 8, 0, 0, 0, 0, 2, 6, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 INSERT INTO `skill_ranks` VALUES(26, 'marksmanship', 9, 0, 0, 0, 0, 6, 0, 10, 0, 0, 2, 0, 7, 0, 0, 0, 4, 0, 0, 0, 0, 0);
