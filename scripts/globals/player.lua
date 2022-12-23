96a2c7d26452a43dd5edae6cb3490f60095efd0e
Aug Shelakh <augshelakh@gmail.com>
2022-12-23 14:40:26 -0500

    Formatting

--- scripts/globals/player.lua
+++ scripts/globals/player.lua
@@ -230,10 +230,10 @@ function onGameIn(player, firstLogin, zoning)
         hookOnGameIn(player, firstLogin, zoning)
     end
     
-        -- CoP mission 3-3B
-        if player:hasKeyItem(tpz.ki.MIMEO_JEWEL) then
-            player:delKeyItem(tpz.ki.MIMEO_JEWEL)
-        end
+    -- CoP mission 3-3B
+    if player:hasKeyItem(tpz.ki.MIMEO_JEWEL) then
+        player:delKeyItem(tpz.ki.MIMEO_JEWEL)
+    end
         
     -- remember time player zoned in (e.g., to support zone-in delays)
     player:setLocalVar("ZoneInTime", os.time())
