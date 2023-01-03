20c8d6ac33062b84627a1438077a42f171630102
lbegnaud <lbegnaud@radersolutions.com>
2023-01-02 20:47:36 -0600

    code cleanup

--- scripts/globals/events/zeni_fest.lua
+++ scripts/globals/events/zeni_fest.lua
@@ -32,22 +32,20 @@ zeni_fest.onSanrakuPlateTradeComplete = function(player, zeni_base)
         player:setCharVar("[ZENI_FEST]TotalZeniBonus", 0)
     end
 
-    local totalBonusThisPeriod = player:getCharVar("[ZENI_FEST]TotalZeniBonus")
     -- give bonus zeni if not a default plate
-    local normalizedMaxZeni = 150
-    local zeniPercOfMax = 0 zeni_base > 5 and (zeni_base / normalizedMaxZeni) or 1
-    printf("zeni %f", zeni_fest.params.MaxBonusZeni)
-    -- bonus zeni is half the distance to the max
-    local bonusZeni = utils.clamp(math.floor(normalizedMaxZeni * (.5) * (1 - zeniPercOfMax)), 0, zeni_fest.params.MaxBonusZeni - totalBonusThisPeriod)
-    local totalZeni = zeni_base + bonusZeni
-    local bonusISP = math.floor(totalZeni * zeni_fest.params.PercOfZeniAsBonusISP / 100)
-
-    player:setCharVar("[ZENI_FEST]LastZeniBonus", now)
-    player:setCharVar("[ZENI_FEST]TotalZeniBonus", totalBonusThisPeriod + bonusZeni)
-    player:addCurrency("zeni_point", bonusZeni)
-    player:addCurrency("imperial_standing", bonusISP)
-    if totalZeni > 10 or bonusZeni > 0 then
-        player:PrintToPlayer(string.format("Sanraku : ZeniFest is currently active. You get %u bonus zeni and %u bonus ISP!", bonusZeni, bonusISP), 0xD)
+    if zeni_base > 5 then
+        local totalBonusThisPeriod = player:getCharVar("[ZENI_FEST]TotalZeniBonus")
+        local normalizedMaxZeni = 150
+        local zeniPercOfMax = zeni_base / normalizedMaxZeni
+        -- bonus zeni is half the distance to the normalized max
+        local bonusZeni = utils.clamp(math.floor(normalizedMaxZeni * (.5) * (1 - zeniPercOfMax)), 0, zeni_fest.params.MaxBonusZeni - totalBonusThisPeriod)
+        local totalZeni = zeni_base + bonusZeni
+        local bonusISP = math.floor(totalZeni * zeni_fest.params.PercOfZeniAsBonusISP / 100)
+        player:setCharVar("[ZENI_FEST]LastZeniBonus", now)
+        player:setCharVar("[ZENI_FEST]TotalZeniBonus", totalBonusThisPeriod + bonusZeni)
+        player:addCurrency("zeni_point", bonusZeni)
+        player:addCurrency("imperial_standing", bonusISP)
+        player:PrintToPlayer(string.format("Sanraku : ZeniFest is currently active. You get %u bonus zeni and %u bonus ISP! %u remaining bonus Zeni during this ZeniFest.", bonusZeni, bonusISP, zeni_fest.params.MaxBonusZeni - totalBonusThisPeriod - bonusZeni), 0xD)
     else
         player:PrintToPlayer(string.format("Sanraku : ZeniFest is currently active. Turn in higher-tier Soul Plates for the bonus.", bonusZeni, bonusISP), 0xD)
     end
