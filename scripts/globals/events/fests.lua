408a2a839e380ee9e91f64b05ad876ce18c4ff69
lbegnaud <lbegnaud@radersolutions.com>
2023-01-05 13:25:27 -0600

    Create fest framework and convert zeni_fest to use it

---
+++ scripts/globals/events/fests.lua
@@ -0,0 +1,19 @@
+---------------------------------------------------------------
+-- Meta file for managing fests                           --
+--(C) 2022 Wings Project. Coded by MowFord.                 --
+---------------------------------------------------------------
+
+require("scripts/globals/settings")
+require("scripts/globals/status")
+require("scripts/globals/msg")
+
+festsData = {
+    [1] = {
+        name = "Exp",
+        maxLengthHours = 72,
+    },
+    [2] = {
+        name = "Zeni",
+        maxLengthHours = 72,
+    }
+}
