b28f2282f2b59c0526f926c03c7c09f0dc291cba
lbegnaud <lbegnaud@radersolutions.com>
2023-02-06 13:43:39 -0600

    call wyvern oddities

--- scripts/globals/mobskills/call_wyvern.lua
+++ scripts/globals/mobskills/call_wyvern.lua
@@ -6,11 +6,16 @@ require("scripts/globals/msg")
 ---------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
+    -- if drg mob has no pet just show no animation (only useed as a 2-hour)
+    if mob:getPet() == nil then
+        skill:setAnim(0)
+    end
     return 0
 end
 
 function onMobWeaponSkill(target, mob, skill)
     if mob:hasPet() == true then
+        skill:setMsg(tpz.msg.basic.NONE)
         return 1
     else
         mob:spawnPet()
