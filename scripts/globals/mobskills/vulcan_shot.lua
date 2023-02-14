4ddd2f46e296dd0b14099fe99a7a6e6dc60f8e30
Server Dingo <serverdingo@gmail.com>
2023-02-13 22:04:09 -0600

    Ashu Talif Captain no longer spams the chat and no longer does ludicrous damage when using Vulcan Shot

--- scripts/globals/mobskills/vulcan_shot.lua
+++ scripts/globals/mobskills/vulcan_shot.lua
@@ -18,7 +18,7 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = 8
+    local dmgmod = 4
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*4, tpz.magic.ele.FIRE, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_WIPE_SHADOWS)
 
