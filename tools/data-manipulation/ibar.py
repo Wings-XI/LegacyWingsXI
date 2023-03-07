1da592941945ab62656b40e53ef93adcf31b8704
Aug Shelakh <augshelakh@gmail.com>
2023-03-07 00:57:47 -0500

    phys/mag formatting

--- tools/data-manipulation/ibar.py
+++ tools/data-manipulation/ibar.py
@@ -52,6 +52,8 @@ def all_mobs(cur):
 
     for zone in zones:
         id = zone[0]
+        if id != 124:
+            continue
 
         cur.execute('SELECT mobid,mobname,mJob,sJob,minlevel,maxlevel,behavior,aggro,detects,true_detection,links,mobtype & 2 = 2,spawntype,respawntime,\
                 (SELECT group_concat(DISTINCT sortname) FROM mob_droplist as d LEFT JOIN item_basic i ON d.itemid = i.itemid WHERE d.dropId = mob_groups.dropid AND d.dropType IN (0,1) order by grouprate,itemrate),\
@@ -67,8 +69,8 @@ def all_mobs(cur):
         file.write(' *\tTotal\t::\t{}\n'.format(len(rows)))
         file.write(']]--\n')
         file.write('\tmb_data = {}\n')
-        count = 1
-        for row in rows:
+ 
+        for count, row in enumerate(rows):
             aggroList = ''
             aggro = int(0 if row[8] is None else row[8])
             if aggro & 1 == 1:
@@ -89,43 +91,58 @@ def all_mobs(cur):
                 aggroList = aggroList + ',JA'
             if aggro & 0x100 == 0x100:
                 aggroList = aggroList + ',SC'
-
-            weakPhys = ''
-            strongPhys = ''
-
+            
+            
             phys = {16:'Slash',17:'Pierce',18:'H2H',19:'Blunt'}
-            for id in phys:
-                val = 1 if row[id] is None else row[id]
-                if val != 1:
-                    if val < 1:
-                        strongPhys += ',' + phys[id]
-                    else:
-                        weakPhys += ',' + phys[id]
-
-            if strongPhys.count(',') == len(phys):
-                strongPhys = ',Physical'
-
-            weakMag = ''
-            strongMag = ''
+            
+            weakPhys = { phys[i]: abs(row[i]-1) for i in range(16, 20) if (type(row[i]) == float and row[i] > 1) }
+            weakPhys = sorted(weakPhys.items(), key=lambda x:x[1], reverse=True)
+            
+            strongPhys = { phys[i]: abs(row[i]-1) for i in range(16, 20) if (type(row[i]) == float and row[i] < 1) }
+            
+            if len(strongPhys) == len(phys):
+                strongPhys = [('Physical', 1)]
+            else:
+                strongPhys = sorted(strongPhys.items(), key=lambda x:x[1], reverse=True)
 
             mag = {20:'Fire',21:'Ice',22:'Wind',23:'Earth',24:'Lightning',25:'Water',26:'Light',27:'Dark'}
-            for id in mag:
-                val = 1 if row[id] is None else row[id]
-                if val != 1:
-                    if val < 1:
-                        strongMag += ',' + mag[id]
-                    else:
-                        weakMag += ',' + mag[id]
+            
+            weakMag = { mag[i]: abs(row[i]-1) for i in range(20, 28) if type(row[i]) == float and row[i] > 1 }
+            weakMag = sorted(weakMag.items(), key=lambda x:x[1], reverse=True)
+            
+            strongMag = { mag[i]: abs(row[i]-1) for i in range(20, 28) if type(row[i]) == float and row[i] < 1 }
 
-            if strongMag.count(',') == len(mag):
-                strongMag = ',Magic'
-
-            strong = strongPhys + strongMag
-            weak = weakPhys + weakMag
-
+            if len(strongMag) == len(mag):
+                strongMag = [('Magic', 1)]
+            else:
+                strongMag = sorted(strongMag.items(), key=lambda x:x[1], reverse=True)
+            
+            weak = [x[0] for x in weakPhys + weakMag]
+            strong = [x[0] for x in strongPhys + strongMag]
+            
+            print(weak, strong)
+            
             file.write('\tmb_data[{}] = {{ nm="{}", id="{}", name="{}", mj="{}", sj="{}", mlvl="{}-{}", behavior="{}", aggro="{}", links="{}", spawntype="{}", respawntime="{}", items="{}", steal="{}", weak="{}", strong="{}", note="" }}\n'.format(
-                    count, 'Y' if row[11] == 1 else 'N', row[0], row[1], row[2], row[3], row[4], row[5], row[6], aggroList, 'Y' if row[10] == 1 else 'N', row[12], row[13], '' if row[14] is None else row[14].replace('_',' '), '' if row[15] is None else row[15].replace('_',' '), weak.replace(',','',1), strong.replace(',','',1)))
-            count = count + 1
+                    count,                          # row
+                    'Y' if row[11] == 1 else 'N',   # NM?
+                    row[0],                         # id
+                    row[1],                         # name
+                    row[2],                         # Main Job
+                    row[3],                         # Sub Job
+                    row[4],                         # Main level
+                    row[5],                         # Sub level
+                    row[6],                         # Behavior
+                    aggroList,                      # Aggro?
+                    'Y' if row[10] == 1 else 'N',   # Links?
+                    row[12],                        # Spawn type
+                    row[13],                        # Respawn time
+                    '' if row[14] is None else row[14].replace('_',' '), # Drop list
+                    '' if row[15] is None else row[15].replace('_',' '), # Steal list
+                    ', '.join(weak),                # Weak to list
+                    ', '.join(strong),              # Strong to list
+                    )
+                )
+
         file.write("\treturn mb_data;\n")
         file.close()
 
