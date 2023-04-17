bf0f70a41bc751500500d4b6834aef2cc099ba97
Aug Shelakh <augshelakh@gmail.com>
2023-04-17 08:59:22 -0400

    move dispel

--- scripts/globals/mobskills/crossthrash.lua
+++ scripts/globals/mobskills/crossthrash.lua
@@ -20,15 +20,11 @@ function onMobWeaponSkill(target, mob, skill)
     local numhits = 2
     local accmod = 1
     local dmgmod = 3
-    local chance = 50
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
-	if (dmg > 0 and skill:getMsg() ~= 31) then
-        target:tryInterruptSpell(mob, info.hitslanded)
-        if (math.random(100) < chance) then
-            target:dispelStatusEffect()
-        end
-    end
+
+	if (dmg > 0 and skill:getMsg() ~= 31) then target:tryInterruptSpell(mob, info.hitslanded) end
+
     return dmg
 end
--- scripts/zones/Horlais_Peak/mobs/Gerjis.lua
+++ scripts/zones/Horlais_Peak/mobs/Gerjis.lua
@@ -2,13 +2,19 @@
 -- Area: Horlais Peak
 --  Mob: Gerjis
 -- BCNM: Eye of the Tiger
--- TODO: code special attacks Crossthrash and Gerjis' Grip
 -----------------------------------
 require("scripts/globals/status")
 -----------------------------------
 
 function onMobInitialize(mob)
     mob:setMod(tpz.mod.EVA, 50)
+end
+
+function onMobFight(mob, target)
+    local chance = 10
+    if (math.random(100) < chance) then
+        target:dispelStatusEffect()
+    end
 end
 
 function onMobDeath(mob, player, isKiller)
