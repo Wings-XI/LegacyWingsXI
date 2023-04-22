d226a72b6476f4c6c1b8abb368026a2e3db5da09
Aug Shelakh <augshelakh@gmail.com>
2023-04-22 09:59:18 -0400

    gerjis and ollas add effects

--- scripts/zones/Horlais_Peak/mobs/Gerjis.lua
+++ scripts/zones/Horlais_Peak/mobs/Gerjis.lua
@@ -8,27 +8,11 @@ require("scripts/globals/status")
 
 function onMobInitialize(mob)
     mob:setMod(tpz.mod.EVA, 50)
-    mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
+    mob:setMod(tpz.mobMod.ADD_EFFECT, 1)
 end
 
 function onAdditionalEffect(mob, target, damage)
-    local chance = 15
-
-    if
-        damage > 0 and
-        chance >= math.random(100)
-    then
-        local dispelables = {}
-        for i, effect in ipairs(target:getStatusEffects()) do
-            if bit.band(effect:getFlag(), tpz.effectFlag.DISPELABLE) == tpz.effectFlag.DISPELABLE then
-                table.insert(dispelables, effect)
-            end
-        end
-        if #dispelables > 0 then
-            local randomEffect = dispelables[math.random(#dispelables)]:getType()
-            target:delStatusEffect(randomEffect)
-        end
-    end
+    return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.PARALYZE, {chance = 50})
 end
 
 function onMobDeath(mob, player, isKiller)
