2705609b5cd63dc69f83c4cb6a5dfcd8cb894cb8
Aug Shelakh <augshelakh@gmail.com>
2023-04-21 22:55:07 -0400

    gerjis dispel rework

--- scripts/zones/Horlais_Peak/mobs/Gerjis.lua
+++ scripts/zones/Horlais_Peak/mobs/Gerjis.lua
@@ -8,12 +8,26 @@ require("scripts/globals/status")
 
 function onMobInitialize(mob)
     mob:setMod(tpz.mod.EVA, 50)
+    mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
 end
 
-function onMobFight(mob, target)
-    local chance = 10
-    if (math.random(100) < chance) then
-        target:dispelStatusEffect()
+function onAdditionalEffect(mob, target, damage)
+    local chance = 15
+
+    if
+        damage > 0 and
+        chance >= math.random(100)
+    then
+        local dispelables = {}
+        for i, effect in ipairs(target:getStatusEffects()) do
+            if bit.band(effect:getFlag(), tpz.effectFlag.DISPELABLE) == tpz.effectFlag.DISPELABLE then
+                table.insert(dispelables, effect)
+            end
+        end
+        if #dispelables > 0 then
+            local randomEffect = dispelables[math.random(#dispelables)]:getType()
+            target:delStatusEffect(randomEffect)
+        end
     end
 end
 
