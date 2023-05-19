86947ae8a601ba3ae3a30db3c061e8bc1bce517d
MowFord <9451178-MowFord@users.noreply.gitlab.com>
2023-05-19 09:31:23 -0500

    cackle adjusted to apply only one effect at a time

--- scripts/globals/mobskills/cackle.lua
+++ scripts/globals/mobskills/cackle.lua
@@ -1,18 +1,25 @@
 ---------------------------------------------------
 --  Cackle
---  Reduces magical attack, accuracy, and defense of targets in an area of effect.
+--  Reduces magical attack, magical accuracy, or magical defense of targets in an area of effect.
+-- Video evidence that it's only ever one effect applied to all targets in AoE
 ---------------------------------------------------
 require("scripts/globals/monstertpmoves")
 require("scripts/globals/settings")
 require("scripts/globals/status")
 ---------------------------------------------------
 
+local typeEffects = {
+    tpz.effect.MAGIC_ATK_DOWN,
+    tpz.effect.MAGIC_ACC_DOWN,
+    tpz.effect.MAGIC_DEF_DOWN,
+}
+
 function onMobSkillCheck(target, mob, skill)
     -- smaller range to use, but 30 yalm AoE. Effectively cannot out-range the skill
     if mob:checkDistance(target) > 10 then
         return 1
     end
-
+    mob:setLocalVar("cackle-index", math.random(1,#typeEffects))
     return 0
 end
 
@@ -20,18 +27,12 @@ function onMobWeaponSkill(target, mob, skill)
     local power = 40
     local duration = 60
     local returnEffect = tpz.effect.NONE
-    local typeEffect = {
-        tpz.effect.MAGIC_ATK_DOWN,
-        tpz.effect.MAGIC_ACC_DOWN,
-        tpz.effect.MAGIC_DEF_DOWN,
-    }
+    local typeEffect = typeEffects[mob:getLocalVar("cackle-index")]
 
     skill:setMsg(tpz.msg.basic.SKILL_MISS)
-    for i = 1, 3 do
-        if MobStatusEffectMove(mob, target, typeEffect[i], power, 0, duration) == tpz.msg.basic.SKILL_ENFEEB_IS then
-            returnEffect = typeEffect[i]
-            skill:setMsg(tpz.msg.basic.SKILL_ENFEEB_IS)
-        end
+    if MobStatusEffectMove(mob, target, typeEffect, power, 0, duration) == tpz.msg.basic.SKILL_ENFEEB_IS then
+        returnEffect = typeEffect
+        skill:setMsg(tpz.msg.basic.SKILL_ENFEEB_IS)
     end
     return returnEffect
 end
