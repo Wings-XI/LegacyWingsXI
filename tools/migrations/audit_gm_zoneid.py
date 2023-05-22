f51614130a3bc34c3cf1ade373e8b996c6036cd3
MowFord <9451178-MowFord@users.noreply.gitlab.com>
2023-05-22 16:12:52 -0500

    audit_gm zoneid type fix

--- tools/migrations/audit_gm_zoneid.py
+++ tools/migrations/audit_gm_zoneid.py
@@ -12,12 +12,19 @@ def needs_to_run(cur):
     cur.execute("SHOW COLUMNS FROM audit_gm LIKE 'zoneid';")
 
     row = cur.fetchone()
-    if row:
+    # check if zoneid is proper type (originally implemented with wrong type)
+    if row and 'smallint' in row[1]:
         return False
 
     return True
 
 def migrate(cur, db):
-    cur.execute("ALTER TABLE `audit_gm` ADD COLUMN zoneid tinyint(3) UNSIGNED NOT NULL DEFAULT 0;")
+    cur.execute("SHOW COLUMNS FROM audit_gm LIKE 'zoneid';")
+
+    row = cur.fetchone()
+    if row:
+        cur.execute("ALTER TABLE `audit_gm` MODIFY COLUMN zoneid smallint(3) UNSIGNED NOT NULL DEFAULT 0;")
+    else:
+        cur.execute("ALTER TABLE `audit_gm` ADD COLUMN zoneid smallint(3) UNSIGNED NOT NULL DEFAULT 0;")
     db.commit()
 
