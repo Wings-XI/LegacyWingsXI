117faf45b86f07138edde8f259896ba28114a183
MowFord <9451178-MowFord@users.noreply.gitlab.com>
2023-05-22 20:42:11 -0500

    fix pawberry's astral flow usage

--- scripts/zones/Temple_of_Uggalepih/mobs/Crimson-toothed_Pawberry.lua
+++ scripts/zones/Temple_of_Uggalepih/mobs/Crimson-toothed_Pawberry.lua
@@ -19,6 +19,19 @@ end
 function onMobSpawn(mob)
     mob:setMobMod(tpz.mobMod.GIL_MIN, 18000)
     mob:setMobMod(tpz.mobMod.GIL_MAX, 28000)
+    -- Special Attacks: Summons a "Tonberry's Avatar" (Carbuncle) and uses Astral Flow (Searing Light) immediately on spawning and multiple times subsequently
+    tpz.mix.jobSpecial.config(mob, {
+        specials =
+        {
+            {id = tpz.jsa.ASTRAL_FLOW, hpp = 100},
+            {id = tpz.jsa.ASTRAL_FLOW, hpp = 50},
+            {id = tpz.jsa.ASTRAL_FLOW, hpp = 20},
+        },
+    })
+end
+
+function onMobDespawn(mob)
     -- ensure astral flow pet is not spawned stuck somewhere
+    DespawnMob(mob:getID() + 1)
     DespawnMob(mob:getID() + 2)
 end
--- scripts/zones/Temple_of_Uggalepih/mobs/Tonberrys_Avatar.lua
+++ scripts/zones/Temple_of_Uggalepih/mobs/Tonberrys_Avatar.lua
@@ -2,8 +2,15 @@
 -- Area: Temple of Uggalepih
 --  Mob: Tonberry's Avatar
 -----------------------------------
+local ID = require("scripts/zones/Temple_of_Uggalepih/IDs")
 mixins = {require("scripts/mixins/families/avatar")}
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller)
+end
+
+function onMobSpawn(mob)
+    if mob:getID() == ID.mob.CRIMSON_TOOTHED_PAWBERRY + 2 then
+        mob:setModelId(791) -- Carbuncle
+    end
 end
