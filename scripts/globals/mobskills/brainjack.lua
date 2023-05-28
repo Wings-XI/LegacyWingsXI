a0af726f1cf2914cd8995a8634cd801bb154e2ae
MowFord <9451178-MowFord@users.noreply.gitlab.com>
2023-05-27 19:11:40 -0500

    Adjustments after group testing: - brainjack 30s base duration - Lamps no attacks during intermediate phases

--- scripts/globals/mobskills/brainjack.lua
+++ scripts/globals/mobskills/brainjack.lua
@@ -15,16 +15,19 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.CHARM_I
     local power = 0
+    local duration = 90
 
     if (not target:isPC()) then
         skill:setMsg(tpz.msg.basic.SKILL_MISS)
         return typeEffect
+    elseif mob:getFamily() == 316 then -- Pandemonium Warden
+        duration = 30
     end
 
-    local msg = MobStatusEffectMove(mob, target, typeEffect, power, 3, 90)
+    local msg = MobStatusEffectMove(mob, target, typeEffect, power, 3, duration)
     if (msg == tpz.msg.basic.SKILL_ENFEEB_IS) then
         mob:charm(target)
-        target:addStatusEffectEx(tpz.effect.BRAINJACK, 0, 25, 3, 90)
+        target:addStatusEffectEx(tpz.effect.BRAINJACK, 0, 25, 3, duration)
         mob:resetEnmity(target)
     end
     skill:setMsg(msg)
--- scripts/zones/Aydeewa_Subterrane/mobs/Pandemonium_Warden.lua
+++ scripts/zones/Aydeewa_Subterrane/mobs/Pandemonium_Warden.lua
@@ -82,7 +82,7 @@ local mobSpecID  = {      0,   688,      0,   688,      0,   688,      0,    688
 local mobSpellID = {      0,     0,      0,     0,      0,     0,      0,      0,      0,      7,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      2}
 -- pets          corpslight, gears, clight, gears, clight, gears, clight,  gears, clight,MamoolJ, clight, Lamiae, clight, Trolls, clight,   Puks, clight, Dahaks, clight,  Bombs,MiniDverg
 local petModelID = {   1841,  1820,   1841,  1820,   1841,  1820,   1841,   1820,   1841,   1639,   1841,   1643,   1841,   1682,   1841,   1746,   1841,    421,   1841,    281,   1839}
-local petSkillID = {     91,   150,     91,   150,     91,   150,     91,    150,     91,    176,     91,    171,     91,    246,     91,    198,     91,   5009,     91,    300,    316}
+local petSkillID = {      0,   150,      0,   150,      0,   150,      0,    150,      0,    176,      0,    171,      0,    246,      0,    198,      0,   5009,      0,    300,    316}
 local petSpellID = {      0,     0,      0,     0,      0,     0,      0,      0,      0,      2,      0,      3,      0,      1,      0,      0,      0,      0,      0,      0,      2}
 --[[
     Their (pet's) form varies depending on what mob the Warden is currently mimicking:
@@ -106,6 +106,8 @@ local petSpellID = {      0,     0,      0,     0,      0,     0,      0,      0
 ]]
 
 function onMobSpawn(mob)
+    mob:setMod(tpz.mod.STATUSRES, 50)
+
     mob:setMobMod(tpz.mobMod.ALLI_HATE, 30)
     mob:setMobMod(tpz.mobMod.HP_STANDBACK, 0)
     mob:setMod(tpz.mod.DEF, 450)
@@ -437,11 +439,13 @@ function handlePet(mob, newPet, oldPet, target, modelId, phase)
         newPet:setMobMod(tpz.mobMod.SKILL_LIST, petSkills)
         newPet:setSkillList(petSkills)
 
-        newPet:SetAutoAttackEnabled(true)
+        -- Don't autoattack if no mobskills (corpselights don't auto or cast)
         if petSkills > 0 then
             newPet:SetMobAbilityEnabled(true)
+            newPet:SetAutoAttackEnabled(true)
         else
             newPet:SetMobAbilityEnabled(false)
+            newPet:SetAutoAttackEnabled(false)
         end
         -- before final phase, not all lamps in a wave use spells
         if petSpells > 0 and (phase == 21 or math.random(1,2) == 1) then
