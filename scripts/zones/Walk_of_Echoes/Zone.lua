6018418253408313a74715118f4b9d9d9b46c582
MowFord <9451178-MowFord@users.noreply.gitlab.com>
2023-06-05 14:31:28 -0500

    functional Veridical Conflux in graub_s flag CHAMPION_OF_THE_DAWN

--- scripts/zones/Walk_of_Echoes/Zone.lua
+++ scripts/zones/Walk_of_Echoes/Zone.lua
@@ -4,7 +4,8 @@
 --
 -----------------------------------
 local ID = require("scripts/zones/Walk_of_Echoes/IDs")
-require("scripts/globals/conquest")
+require("scripts/globals/quests")
+require("scripts/globals/missions")
 -----------------------------------
 
 function onInitialize(zone)
@@ -12,6 +13,9 @@ end
 
 function onZoneIn(player, prevZone)
     local cs = -1
+    -- WINGSCUSTOM: cait sith avatar quest available when currently-implemented WOTG missions are completed
+    -- This needs to be changed as more missions are implemented
+    local caitSithAvailable = player:hasCompletedMission(WOTG, tpz.mission.id.wotg.PURPLE_THE_NEW_BLACK)
 
     if
         player:getXPos() == 0 and
@@ -19,6 +23,12 @@ function onZoneIn(player, prevZone)
         player:getZPos() == 0
     then
         player:setPos(-420, 10, 0, 69)
+    end
+
+    if prevZone == 89 and -- coming from Grauberg_S
+        caitSithAvailable and
+        player:getQuestStatus(CRYSTAL_WAR, tpz.quest.id.crystalWar.CHAMPION_OF_THE_DAWN) == QUEST_AVAILABLE then
+            cs = 15
     end
 
     return cs
@@ -35,4 +45,7 @@ function onEventUpdate(player, csid, option)
 end
 
 function onEventFinish(player, csid, option)
+    if csid == 15 then
+        player:addQuest(tpz.quest.log_id.CRYSTAL_WAR, tpz.quest.id.crystalWar.CHAMPION_OF_THE_DAWN)
+    end
 end
---
+++ scripts/zones/Walk_of_Echoes/npcs/_521.lua
@@ -0,0 +1,17 @@
+-----------------------------------
+-- Area: Walk Of Echoes
+--  NPC: Ornate Door
+-- !pos -700 -17.6 -327 182
+-----------------------------------
+
+function onTrade(player, npc, trade)
+end
+
+function onTrigger(player, npc)
+end
+
+function onEventUpdate(player, csid, option)
+end
+
+function onEventFinish(player, csid, option)
+end
