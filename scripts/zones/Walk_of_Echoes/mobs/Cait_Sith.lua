8250d25a832926909588df53d8950707c0a9e601
MowFord <9451178-MowFord@users.noreply.gitlab.com>
2023-06-07 14:25:13 -0500

    cait sith fight resistances

--- scripts/zones/Walk_of_Echoes/mobs/Cait_Sith.lua
+++ scripts/zones/Walk_of_Echoes/mobs/Cait_Sith.lua
@@ -8,6 +8,24 @@ local ID = require("scripts/zones/Walk_of_Echoes/IDs")
 
 function onMobSpawn(mob)
     mob:setModelId(28) -- copied modelid from cait sith pet since db value has bunk animations
+
+    -- resistances: https://www.bg-wiki.com/ffxi/Cait_Sith_Prime
+    mob:setMod(tpz.mod.UDMGMAGIC, -50)
+    mob:setMod(tpz.mod.UDMGBREATH, -50)
+    mob:setMod(tpz.mod.SDT_FIRE, 50)
+    mob:setMod(tpz.mod.SDT_ICE, 30)
+    mob:setMod(tpz.mod.SDT_WIND, 50)
+    mob:setMod(tpz.mod.SDT_EARTH, 30)
+    mob:setMod(tpz.mod.SDT_THUNDER, 50)
+    mob:setMod(tpz.mod.SDT_WATER, 30)
+    mob:setMod(tpz.mod.SDT_LIGHT, -100) -- immune
+    mob:setMod(tpz.mod.SDT_DARK, 30)
+
+    -- status resistances: https://ffxiclopedia.fandom.com/wiki/Champion_of_the_Dawn
+    mob:setMod(tpz.mod.STUNRES, -10)
+    mob:setMod(tpz.mod.LULLABYRES, 100)
+    mob:setMod(tpz.mod.SLEEPRES, 100)
+    mob:setMod(tpz.mod.SILENCERES, 100)
 end
 
 function onMobEngaged(mob, player)
