78c790e8d11d6074f37d64c646383b8a95fcb93b
MowFord <9451178-MowFord@users.noreply.gitlab.com>
2023-06-07 22:17:11 -0500

    reduce amnesia duration and change to FIRE

--- scripts/globals/abilities/pets/eerie_eye.lua
+++ scripts/globals/abilities/pets/eerie_eye.lua
@@ -25,12 +25,14 @@ function onPetAbility(target, pet, skill)
     
     local resist = applyResistanceAbility(pet,target,ele,tpz.skill.ENFEEBLING_MAGIC,bonus)
     
+    -- https://wikiwiki.jp/ffxi/%E5%8F%AC%E5%96%9A%E9%AD%94%E6%B3%95
+    -- TL;DR 30s silence and 10s amnesia and amnesia is fire element
     if resist >= 0.5 then --Do it!
         if target:addStatusEffect(tpz.effect.SILENCE, 1, 0, duration * resist) then
             skill:setMsg(tpz.msg.basic.SKILL_ENFEEB_IS)
-            local resist2 = applyResistanceAbility(pet,target,ele,tpz.skill.ENFEEBLING_MAGIC,bonus)
+            local resist2 = applyResistanceAbility(pet,target,tpz.magic.ele.FIRE,tpz.skill.ENFEEBLING_MAGIC,bonus)
             -- WINGSCUSTOM require 2 successful resistance checks (and actual application of SILENCE)
-            if resist * resist2 >= 1 and target:addStatusEffect(tpz.effect.AMNESIA, 1, 0, duration * resist2 / 2) then
+            if resist * resist2 >= 1 and target:addStatusEffect(tpz.effect.AMNESIA, 1, 0, duration * resist2 / 3) then
                 returnEffect = tpz.effect.AMNESIA
             else
                 returnEffect = tpz.effect.SILENCE
