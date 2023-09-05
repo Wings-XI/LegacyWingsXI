23725cdd14898e38a19f032da6043a59bf457a1b
MowFord <9451178-MowFord@users.noreply.gitlab.com>
2023-09-05 13:39:50 -0500

    allow take_dmg listener to recognize blue magic

--- scripts/zones/Talacca_Cove/mobs/Valkeng.lua
+++ scripts/zones/Talacca_Cove/mobs/Valkeng.lua
@@ -105,7 +105,7 @@ function onMobInitialize(mob)
     mob:addListener("TAKE_DAMAGE", "VALKENG_TAKE_DAMAGE", function(mob, amount, attacker, attackType, damageType)
         if attackType == tpz.attackType.PHYSICAL then
             mob:setLocalVar("PhysicalDamage", mob:getLocalVar("PhysicalDamage") + amount)
-        elseif attackType == tpz.attackType.MAGICAL then
+        elseif attackType == tpz.attackType.MAGICAL or mob:getLocalVar("dmgsourceblumagic") == 1 then
             mob:setLocalVar("MagicalDamage", mob:getLocalVar("MagicalDamage") + amount)
         elseif attackType == tpz.attackType.RANGED then
             mob:setLocalVar("RangedDamage", mob:getLocalVar("RangedDamage") + amount)
