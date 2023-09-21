fc6ec41e6283ca3485549825fa710dbff9a81585
MowFord <9451178-MowFord@users.noreply.gitlab.com>
2023-09-21 17:23:42 -0500

    sub-zero smash usage adjustments

--- scripts/zones/Beaucedine_Glacier_[S]/mobs/Ruszor.lua
+++ scripts/zones/Beaucedine_Glacier_[S]/mobs/Ruszor.lua
@@ -5,5 +5,18 @@
 mixins = { require("scripts/mixins/families/ruszor") }
 -----------------------------------
 
+function onMobFight(mob, target)
+    -- If target changes and the new target is behind, use sub-zero smash (with some restrictions)
+    if
+        target ~= nil and
+        mob:getTP() > 2000 * mob:getHPP() / 100 and
+        mob:getLocalVar('lastSmash') < os.time() and
+        target:isBehind(mob, 48)
+    then
+        mob:setLocalVar('nextSmash', os.time() + 15)
+        mob:useMobAbility(2436)
+    end
+end
+
 function onMobDeath(mob, player, isKiller)
 end
