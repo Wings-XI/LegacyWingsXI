43f8ba12b1733233ed725cee03861a4ac646536a
MowFord <9451178-MowFord@users.noreply.gitlab.com>
2023-10-03 05:37:21 +0000

    Revert "Update to retail relic ws modifiers"
    This reverts commit 6cdb97ee10580cfebcc5312fef7b3daa550b608e

--- scripts/globals/weaponskills/catastrophe.lua
+++ scripts/globals/weaponskills/catastrophe.lua
@@ -8,7 +8,7 @@
 -- Aligned with the Shadow Gorget & Soil Gorget.
 -- Aligned with the Shadow Belt & Soil Belt.
 -- Element: None
--- Modifiers: STR:40%  AGI:40%
+-- Modifiers: INT:40%  AGI:40%
 -- 100%TP    200%TP    300%TP
 -- 2.75      2.75      2.75
 -----------------------------------
@@ -22,11 +22,11 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 1
     params.ftp100 = 2.75 params.ftp200 = 2.75 params.ftp300 = 2.75
-    params.str_wsc = 0.4 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.4 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
+    params.str_wsc = 0.0 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.4 params.int_wsc = 0.4 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
     params.acc100 = 0.0 params.acc200 = 0.0 params.acc300 = 0.0
-    params.atk100 = 1 params.atk200 = 1 params.atk300 = 1
+    params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
 
     if USE_ADOULIN_WEAPON_SKILL_CHANGES then
         params.str_wsc = 0.4 params.agi_wsc = 0.0 params.int_wsc = 0.4
--- scripts/globals/weaponskills/tachi_kaiten.lua
+++ scripts/globals/weaponskills/tachi_kaiten.lua
@@ -12,7 +12,7 @@
 -- Aligned with the Breeze Gorget, Thunder Gorget & Light Gorget.
 -- Aligned with the Breeze Belt, Thunder Belt & Light Belt.
 -- Element: None
--- Modifiers: STR:80%
+-- Modifiers: STR:75%
 -- 100%TP    200%TP    300%TP
 -- 3.00      3.00      3.00
 -----------------------------------
@@ -26,7 +26,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 1
     params.ftp100 = 3 params.ftp200 = 3 params.ftp300 = 3
-    params.str_wsc = 0.8 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
+    params.str_wsc = 0.6 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
     params.acc100 = 0.0 params.acc200 = 0.0 params.acc300 = 0.0
